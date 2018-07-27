98e10ff13b5c67dae74516c4544bdb6aaf0405f3
Andrew Brogdon <brogdon@gmail.com>
2018-07-27 10:10:53 -0700

    Adding Shrine to travis script.

--- shrine/test/main_test.dart
+++ shrine/test/main_test.dart
@@ -1,1 +1,11 @@
-void main() {}
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Unit tests', () {
+    test('This test always passes', () {});
+  });
+}
--- travis_script.sh
+++ travis_script.sh
@@ -1,9 +1,16 @@
 set -e
 
-echo "== Testing 'jsonexample' on Flutter's $FLUTTER_VERSION channel =="
+declare -a  PROJECT_NAMES=(
+	"jsonexample" \
+	"shrine" \
+)
 
-pushd jsonexample
-../flutter/bin/flutter test
-popd
+for PROJECT_NAME in "${PROJECT_NAMES[@]}"
+do
+	echo "== Testing '${PROJECT_NAME}' on Flutter's $FLUTTER_VERSION channel =="
+	pushd "${PROJECT_NAME}"
+	../flutter/bin/flutter test
+	popd
+done
 
 echo "-- Success --"
