370a72caa56c2d5b3af2f84644a3d7dd9ffb91ae
Andrew Brogdon <RedBrogdon@users.noreply.github.com>
2018-08-08 08:41:18 -0700

    Fixing unchangeable Shrine categories. (#9)

--- shrine/lib/main.dart
+++ shrine/lib/main.dart
@@ -13,12 +13,16 @@
 // limitations under the License.
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 import 'app.dart';
 import 'model/app_state_model.dart';
 
 void main() {
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
   AppStateModel model = AppStateModel();
   model.loadProducts();
 
