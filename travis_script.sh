b45efc2ce15f47e15ab10cdd37b5a59e19c8b711
Andrew Brogdon <brogdon@gmail.com>
2018-11-07 13:57:49 -0800

    Updating travis script.

--- travis_script.sh
+++ travis_script.sh
@@ -10,6 +10,8 @@ for PROJECT_NAME in "${PROJECT_NAMES[@]}"
 do
 	echo "== Testing '${PROJECT_NAME}' on Flutter's $FLUTTER_VERSION channel =="
 	pushd "${PROJECT_NAME}"
+	../flutter/bin/flutter analyze
+	../flutter/bin/flutter format -n
 	../flutter/bin/flutter test
 	popd
 done
