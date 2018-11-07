eab91927f78d87d9343c3ceebdc340c2ee13bcc8
Andrew Brogdon <brogdon@gmail.com>
2018-11-07 09:54:40 -0800

    Fixes Future<void> issue in Shrine.

--- shrine/lib/expanding_bottom_sheet.dart
+++ shrine/lib/expanding_bottom_sheet.dart
@@ -381,7 +381,8 @@ class _ExpandingBottomSheetState extends State<ExpandingBottomSheet> with Ticker
   // only be relevant for Android).
   Future<bool> _onWillPop() async {
     if (!_isOpen) {
-      return SystemNavigator.pop();
+      await SystemNavigator.pop();
+      return true;
     }
 
     close();
