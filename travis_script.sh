1f5a87b873cad0caf89d27262479c3a774f60895
Andrew Brogdon <brogdon@gmail.com>
2018-11-08 10:10:32 -0800

    Fixing format command in travis script.

--- travis_script.sh
+++ travis_script.sh
@@ -10,9 +10,16 @@ for PROJECT_NAME in "${PROJECT_NAMES[@]}"
 do
 	echo "== Testing '${PROJECT_NAME}' on Flutter's $FLUTTER_VERSION channel =="
 	pushd "${PROJECT_NAME}"
+
+	# Run the analyzer to find any static analysis issues.
 	../flutter/bin/flutter analyze
-	../flutter/bin/flutter format -n
+
+	# Run the formatter on all the dart files to make sure everything's linted.
+	find . | grep "\.dart$" | xargs /flutter/bin/flutter format -n
+
+	# Run the actual tests.
 	../flutter/bin/flutter test
+
 	popd
 done
 
