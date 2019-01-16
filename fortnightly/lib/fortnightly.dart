793bd1dc42e2d18ecc2a3c74f738eb2a64dec51d
Will Larche <larche@google.com>
2019-01-15 21:50:37 -0800

    [Fortnightly] Moving app logic into demo.

--- fortnightly/lib/fortnightly.dart
+++ fortnightly/lib/fortnightly.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/material.dart';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Typography Demo',
+      home: FortnightlyDemo(),
+      theme: _kFortnightlyTheme,
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
 class FortnightlyDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
