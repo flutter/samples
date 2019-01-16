e8206096d91d25466bee19081082c35fb0e9d2e5
Will Larche <larche@google.com>
2019-01-15 21:48:14 -0800

    [Fortnightly] Moving theming into same file as demo.

--- fortnightly/lib/fortnightly.dart
+++ fortnightly/lib/fortnightly.dart
@@ -130,3 +130,58 @@ class FruitPage extends StatelessWidget {
     );
   }
 }
+
+final ThemeData _kFortnightlyTheme = _buildFortnightlyTheme();
+
+ThemeData _buildFortnightlyTheme() {
+  final ThemeData base = ThemeData.light();
+  return base.copyWith(
+    primaryTextTheme: _buildTextTheme(base.primaryTextTheme),
+  );
+}
+
+TextTheme _buildTextTheme(TextTheme base) {
+  TextTheme theme = base.apply(bodyColor: Colors.black);
+  theme = theme.apply(displayColor: Colors.black);
+
+  theme = theme.copyWith(
+    display1: base.display1.copyWith(
+      fontFamily: 'Merriweather',
+      fontStyle: FontStyle.italic,
+      fontSize: 28.0,
+      fontWeight: FontWeight.w800,
+      color: Colors.black,
+      height: .88,
+    ),
+    display3: base.display3.copyWith(
+      fontFamily: 'LibreFranklin',
+      fontSize: 18.0,
+      fontWeight: FontWeight.w500,
+      color: Colors.black.withAlpha(153),
+    ),
+    headline: base.headline.copyWith(fontWeight: FontWeight.w500),
+    body1: base.body1.copyWith(
+      fontFamily: 'Merriweather',
+      fontSize: 14.0,
+      fontWeight: FontWeight.w300,
+      color: Color(0xFF666666),
+      height: 1.11,
+    ),
+    body2: base.body2.copyWith(
+      fontFamily: 'Merriweather',
+      fontSize: 16.0,
+      fontWeight: FontWeight.w300,
+      color: Color(0xFF666666),
+      height: 1.4,
+      letterSpacing: .25,
+    ),
+    overline: TextStyle(
+      fontFamily: 'LibreFranklin',
+      fontSize: 10.0,
+      fontWeight: FontWeight.w700,
+      color: Colors.black,
+    ),
+  );
+
+  return theme;
+}
