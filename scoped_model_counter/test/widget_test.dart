7e606f18ff32c955202c819d0b6cca92e33c5e09
Filip Hracek <filip.hracek@gmail.com>
2019-05-16 10:58:28 -0700

    Migrate from scoped_model to provider
    This is the minimal possible change. It is meant to be readable as a standalone Pull Request.
    
    This does not touch comments. Additionally, it doesn’t use the niceties of provider, like automatic disposal of the model. Lastly, it doesn’t even rename the project from `scoped_model_counter`. All that will come in a follow-up PR.

--- scoped_model_counter/test/widget_test.dart
+++ scoped_model_counter/test/widget_test.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:scoped_model/scoped_model.dart';
+import 'package:provider/provider.dart';
 import 'package:scoped_model_counter/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app, provide it with a model, and trigger a frame.
     await tester.pumpWidget(
-      ScopedModel(
-        model: Counter(),
+      ChangeNotifierProvider(
+        builder: (_) => Counter(),
         child: MyApp(),
       ),
     );
