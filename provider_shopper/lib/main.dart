9c27d6f4ef7ad5ee43574aaaa6c6220151d77e1e
Filip Hracek <filip.hracek@gmail.com>
2019-05-21 13:26:44 -0700

    Simplify Provider at the top of the app

--- provider_shopper/lib/main.dart
+++ provider_shopper/lib/main.dart
@@ -6,40 +6,35 @@ import 'package:provider_shopper/screens/catalog.dart';
 import 'package:provider_shopper/screens/login.dart';
 
 void main() {
-  runApp(
-    ChangeNotifierProvider(
-      builder: (context) => CartModel(),
-      child: ChangeNotifierProvider(
-        builder: (context) => CartModel(),
-        child: MyApp(),
-      ),
-    ),
-  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.yellow,
-        textTheme: TextTheme(
-          display4: TextStyle(
-            fontFamily: 'Corben',
-            fontWeight: FontWeight.w700,
-            fontSize: 24,
-            color: Colors.black,
+    return ChangeNotifierProvider(
+      builder: (context) => CartModel(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.yellow,
+          textTheme: TextTheme(
+            display4: TextStyle(
+              fontFamily: 'Corben',
+              fontWeight: FontWeight.w700,
+              fontSize: 24,
+              color: Colors.black,
+            ),
           ),
         ),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => MyLoginScreen(),
+          '/catalog': (context) => MyCatalog(),
+          '/cart': (context) => MyCart(),
+        },
       ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => MyLoginScreen(),
-        '/catalog': (context) => MyCatalog(),
-        '/cart': (context) => MyCart(),
-      },
     );
   }
 }
