b52ad2d43615720ec443d2e346ceffa290a72072
Andrew Brogdon <redbrogdon@google.com>
2019-06-25 10:46:44 -0700

    Soft tabs in travis script.

--- travis_script.sh
+++ travis_script.sh
@@ -3,30 +3,30 @@ set -e
 declare -a  PROJECT_NAMES=(
     "flutter_maps_firestore" \
     "isolate_example" \
-	"jsonexample" \
-	"place_tracker" \
+    "jsonexample" \
+    "place_tracker" \
     "platform_design"
-	"platform_view_swift" \
-	"provider_counter" \
-	"shrine" \
-	"veggieseasons" \
+    "platform_view_swift" \
+    "provider_counter" \
+    "shrine" \
+    "veggieseasons" \
 )
 
 for PROJECT_NAME in "${PROJECT_NAMES[@]}"
 do
-	echo "== Testing '${PROJECT_NAME}' on Flutter's $FLUTTER_VERSION channel =="
-	pushd "${PROJECT_NAME}"
+    echo "== Testing '${PROJECT_NAME}' on Flutter's $FLUTTER_VERSION channel =="
+    pushd "${PROJECT_NAME}"
 
-	# Run the analyzer to find any static analysis issues.
-	../flutter/bin/flutter analyze
+    # Run the analyzer to find any static analysis issues.
+    ../flutter/bin/flutter analyze
 
-	# Run the formatter on all the dart files to make sure everything's linted.
-	../flutter/bin/flutter format -n --set-exit-if-changed .
+    # Run the formatter on all the dart files to make sure everything's linted.
+    ../flutter/bin/flutter format -n --set-exit-if-changed .
 
-	# Run the actual tests.
-	../flutter/bin/flutter test
+    # Run the actual tests.
+    ../flutter/bin/flutter test
 
-	popd
+    popd
 done
 
 echo "-- Success --"
