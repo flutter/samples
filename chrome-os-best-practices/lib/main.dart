7c17fe043ec2a1096a0ef1e53a72afe2aeb4a6bd
Brett Morgan <brettmorgan@google.com>
2019-06-28 16:16:40 +1000

    One more additional trailing comma

--- chrome-os-best-practices/lib/main.dart
+++ chrome-os-best-practices/lib/main.dart
@@ -240,8 +240,10 @@ class ChatMessage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizeTransition(
-      sizeFactor:
-          CurvedAnimation(parent: animationController, curve: Curves.easeOut),
+      sizeFactor: CurvedAnimation(
+        parent: animationController,
+        curve: Curves.easeOut,
+      ),
       axisAlignment: 0.0,
       child: Container(
         margin: const EdgeInsets.symmetric(vertical: 10.0),
