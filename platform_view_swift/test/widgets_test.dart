3085b6de45e6d5952bb42beb30a5dddb0ae5ea61
Brett Morgan <brett.morgan@gmail.com>
2019-07-03 08:49:17 +1000

    Analysis options

--- platform_view_swift/test/widgets_test.dart
+++ platform_view_swift/test/widgets_test.dart
@@ -7,5 +7,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('This test always passes', (WidgetTester tester) async {});
+  testWidgets('This test always passes', (tester) async {});
 }
