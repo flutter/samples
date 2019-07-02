4f72ab1776af52e92b24e163c43592850e2aaeed
Brett Morgan <brett.morgan@gmail.com>
2019-07-03 08:08:35 +1000

    Add analysis_options, re-format, add commas. (#101)

--- chrome-os-best-practices/test/widget_test.dart
+++ chrome-os-best-practices/test/widget_test.dart
@@ -11,7 +11,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:friendlychat/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Counter increments smoke test', (tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(FriendlychatApp());
 
