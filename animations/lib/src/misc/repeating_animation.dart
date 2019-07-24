1bbd494aeff245e7afdd7200f6d2eb08be77c5c8
John Ryan <ryjohn@google.com>
2019-07-24 15:54:05 -0700

    move non-updating widgets out of AnimatedBuilder

--- animations/lib/src/misc/repeating_animation.dart
+++ animations/lib/src/misc/repeating_animation.dart
@@ -32,23 +32,23 @@ class RepeatingAnimationDemoState extends State<RepeatingAnimationDemo>
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: _borderRadius,
-      builder: (context, child) {
-        return Scaffold(
-          appBar: AppBar(title: Text('Repeating Animation')),
-          body: Center(
-            child: Container(
+    return Scaffold(
+      appBar: AppBar(title: Text('Repeating Animation')),
+      body: Center(
+        child: AnimatedBuilder(
+          animation: _borderRadius,
+          builder: (context, child) {
+            return Container(
               width: 200,
               height: 200,
               decoration: BoxDecoration(
                 color: Colors.deepPurple,
                 borderRadius: _borderRadius.value,
               ),
-            ),
-          ),
-        );
-      },
+            );
+          },
+        ),
+      ),
     );
   }
 
