45f33f9165194c1b0e4aa7d1335d96f445a1257c
Andrew Brogdon <redbrogdon@google.com>
2019-08-14 10:31:46 -0700

    Updating travis script to find flutter reliably.

--- travis_script.sh
+++ travis_script.sh
@@ -1,5 +1,17 @@
 set -e
 
+# Backs up one directory at a time, looking for one called "flutter".
+function getFlutterPath() {
+    local path=".."
+    local counter=0
+
+    while [[ "${counter}" -lt 10 ]]; do
+        [ -d "${path}/flutter" ] && echo "${path}/flutter" && return 0
+        let counter++
+        path="${path}/.."
+    done
+}
+
 declare -a  PROJECT_NAMES=(
     "animations" \
     "flutter_maps_firestore" \
@@ -19,14 +31,22 @@ do
     echo "== Testing '${PROJECT_NAME}' on Flutter's $FLUTTER_VERSION channel =="
     pushd "${PROJECT_NAME}"
 
+    localSdkPath=$(getFlutterPath)
+    
+    if [ -z "$localSdkPath" ]
+    then 
+        echo "Failed to find Flutter SDK for '${PROJECT_NAME}'."
+        exit 1
+    fi
+
     # Run the analyzer to find any static analysis issues.
-    ../flutter/bin/flutter analyze
+    "${localSdkPath}/bin/flutter" analyze
 
     # Run the formatter on all the dart files to make sure everything's linted.
-    ../flutter/bin/flutter format -n --set-exit-if-changed .
+    "${localSdkPath}/bin/flutter" format -n --set-exit-if-changed .
 
     # Run the actual tests.
-    ../flutter/bin/flutter test
+    "${localSdkPath}/bin/flutter" test
 
     popd
 done
