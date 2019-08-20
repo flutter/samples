0363571da88b9aa129c8bbbd32eb682dc112bcf4
Andrew Brogdon <redbrogdon@google.com>
2019-08-20 15:06:26 -0700

    Halfway through updating

---
+++ veggieseasons/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/redbrogdon/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/redbrogdon/source/samples/veggieseasons"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/redbrogdon/flutter/bin/cache/artifacts/engine/ios-release"
+export "FLUTTER_BUILD_NAME=1.1.0"
+export "FLUTTER_BUILD_NUMBER=1.1.0"
