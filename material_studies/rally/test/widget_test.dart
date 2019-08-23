b8597a3a99bd8d787b6fc465d8104fbba59ab3d9
Lisa Liao <lisaliao@google.com>
2019-08-23 13:26:24 -0700

    Address PR Feedback

--- material_studies/rally/test/widget_test.dart
+++ material_studies/rally/test/widget_test.dart
@@ -11,20 +11,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:rally/app.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('Smoke test', (WidgetTester tester) async {
     await tester.pumpWidget(RallyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.byType(MaterialApp), findsOneWidget);
   });
 }
