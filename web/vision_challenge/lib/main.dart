4894bf14b597e48593f4cb3048f150b20223c628
John Ryan <ryjohn@google.com>
2019-08-29 09:56:31 -0700

    update vision_challenge

--- web/vision_challenge/lib/main.dart
+++ web/vision_challenge/lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:flutter_web/material.dart';
-import 'package:vision_challenge/packages/flutter_redux.dart';
-import 'package:vision_challenge/packages/redux.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
 import 'game.dart';
 
 void main() {
