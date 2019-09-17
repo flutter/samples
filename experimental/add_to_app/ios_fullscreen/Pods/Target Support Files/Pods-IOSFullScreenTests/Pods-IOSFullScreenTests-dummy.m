3853fa37658cf8b61d1724d05a35d1a8e3fde1a2
Andrew Brogdon <redbrogdon@google.com>
2019-09-17 09:24:55 -0700

    Removed gitignored files

--- experimental/add_to_app/ios_fullscreen/Pods/Target Support Files/Pods-IOSFullScreenTests/Pods-IOSFullScreenTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_IOSFullScreenTests : NSObject
-@end
-@implementation PodsDummy_Pods_IOSFullScreenTests
-@end
--- experimental/add_to_app/ios_fullscreen/Pods/Target Support Files/Pods-IOSFullScreenUITests/Pods-IOSFullScreenUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_IOSFullScreenUITests : NSObject
-@end
-@implementation PodsDummy_Pods_IOSFullScreenUITests
-@end
