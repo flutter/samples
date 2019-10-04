43b6de0cdb809d61a3ba9f61a3fb39e5416fe1e2
Andrew Brogdon <redbrogdon@google.com>
2019-10-04 11:30:27 -0700

    Whitespace in iOS code

--- experimental/add_to_app/ios_fullscreen/IOSFullScreen/ViewController.swift
+++ experimental/add_to_app/ios_fullscreen/IOSFullScreen/ViewController.swift
@@ -6,13 +6,13 @@ import UIKit
 import Flutter
 
 class ViewController: UIViewController {
-    
+
     @IBOutlet weak var counterLabel: UILabel!
-    
+
     var methodChannel : FlutterMethodChannel?
-    
+
     var count = 0
-    
+
     override func viewDidLoad() {
         if let flutterEngine = (UIApplication.shared.delegate as? AppDelegate)?.flutterEngine {
             methodChannel = FlutterMethodChannel(name: "dev.flutter.example/counter",
@@ -31,18 +31,16 @@ class ViewController: UIViewController {
                     print("Unrecognized method name: \(call.method)")
                 }
             })
-
         }
     }
-    
+
     func reportCounter() {
         methodChannel?.invokeMethod("reportCounter", arguments: count)
     }
-    
+
     @IBAction func buttonWasTapped(_ sender: Any) {
         let flutterEngine = (UIApplication.shared.delegate as? AppDelegate)?.flutterEngine
         let flutterViewController = FlutterViewController(engine: flutterEngine, nibName: nil, bundle: nil)!
         self.present(flutterViewController, animated: false, completion: nil)
     }
 }
-
