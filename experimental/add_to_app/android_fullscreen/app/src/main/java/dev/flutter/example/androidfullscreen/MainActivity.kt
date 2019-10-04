ff230fe6819998881070e7a7b6ee85321b148683
Andrew Brogdon <redbrogdon@google.com>
2019-10-04 11:27:35 -0700

    Working Android

--- experimental/add_to_app/android_fullscreen/app/src/main/java/dev/flutter/example/androidfullscreen/MainActivity.kt
+++ experimental/add_to_app/android_fullscreen/app/src/main/java/dev/flutter/example/androidfullscreen/MainActivity.kt
@@ -9,31 +9,68 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.FlutterEngineCache
+import io.flutter.embedding.engine.dart.DartExecutor
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.view.FlutterMain
 
 class MainActivity : AppCompatActivity() {
 
+    private val ENGINE_ID = "1"
+
+    private var channel : MethodChannel? = null
+    private var count = 0
     private var counterLabel : TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        FlutterMain.ensureInitializationComplete(applicationContext, null)
+
+        // Create and warm up Flutter engine.
+        val flutterEngine = FlutterEngine(this)
+
+        flutterEngine
+            .dartExecutor
+            .executeDartEntrypoint(
+                DartExecutor.DartEntrypoint.createDefault()
+            )
+
+        FlutterEngineCache.getInstance().put(ENGINE_ID, flutterEngine)
+
+        // Set up the method channel that will pass data to and from the Flutter module.
+        channel = MethodChannel(flutterEngine.dartExecutor, "dev.flutter.example/counter")
+
+        channel?.setMethodCallHandler { call, _ ->
+            when (call.method) {
+                "incrementCounter" -> {
+                    count++
+                    counterLabel?.text = "Current counter: $count"
+                    reportCounter()
+                }
+                "requestCounter" -> {
+                    print("requestCounter")
+                    reportCounter()
+                }
+            }
+        }
+
+        // Set up label and button.
         counterLabel = findViewById(R.id.counter_label)
         val button = findViewById<Button>(R.id.launch_button)
 
         button.setOnClickListener {
-            val intent = FlutterActivity.createDefaultIntent(this)
+            val intent = FlutterActivity
+                .withCachedEngine(ENGINE_ID)
+                .build(this)
+
             startActivity(intent)
-            //            val intent = Intent(this, MyFlutterActivity::class.java)
-//            startActivityForResult(intent, 1)
         }
     }
 
-//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//        super.onActivityResult(requestCode, resultCode, data)
-//        if (data != null) {
-//            val counter = data.getIntExtra("counter", 0)
-//            counterLabel?.text = "Current counter: $counter"
-//        }
-//    }
+    private fun reportCounter() {
+        channel?.invokeMethod("reportCounter", count)
+    }
 }
