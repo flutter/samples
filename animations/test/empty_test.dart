74c1777b2abef9248f2de6cb173aa64f6596690b
Thea Flowers <theaflowers@google.com>
2019-10-07 14:57:12 -0700

    touch ./animations (#148)
    * Update project structure using flutter create and remove unused tests
    * Small updates to main.dart
    * Update maintenance.md
    * Add missing newline
    * Add rudimentary test
    * Apply formatting

--- animations/test/empty_test.dart
+++
@@ -1,9 +0,0 @@
-// Copyright 2019 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  testWidgets('This test always passes', (tester) async {});
-}
