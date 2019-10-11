947042d8df46943dda4b02cacfa6c9418c046b11
Andrew Brogdon <RedBrogdon@users.noreply.github.com>
2019-10-11 15:17:16 -0700

    Adds data transfer to Add2App samples, using caching on Android (#146)

--- experimental/add_to_app/android_fullscreen/app/src/main/java/dev/flutter/example/androidfullscreen/MainActivity.kt
+++ experimental/add_to_app/android_fullscreen/app/src/main/java/dev/flutter/example/androidfullscreen/MainActivity.kt
@@ -6,18 +6,32 @@ package dev.flutter.example.androidfullscreen
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : AppCompatActivity() {
+    private var counterLabel: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        counterLabel = findViewById(R.id.counter_label)
+
         val button = findViewById<Button>(R.id.launch_button)
+
         button.setOnClickListener {
-            startActivity(FlutterActivity.createDefaultIntent(this))
+            val intent = FlutterActivity
+                .withCachedEngine(ENGINE_ID)
+                .build(this)
+            startActivity(intent)
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val app = application as MyApplication
+        counterLabel?.text = "Current count: ${app.count}"
     }
 }
--- experimental/add_to_app/ios_fullscreen/IOSFullScreen/ViewController.swift
+++ experimental/add_to_app/ios_fullscreen/IOSFullScreen/ViewController.swift
@@ -6,10 +6,43 @@ import UIKit
 import Flutter
 
 class ViewController: UIViewController {
+
+    @IBOutlet weak var counterLabel: UILabel!
+
+    var methodChannel : FlutterMethodChannel?
+    var count = 0
+
+    override func viewDidLoad() {
+        if let flutterEngine = (UIApplication.shared.delegate as? AppDelegate)?.flutterEngine {
+            methodChannel = FlutterMethodChannel(name: "dev.flutter.example/counter",
+                                                 binaryMessenger: flutterEngine.binaryMessenger)
+            methodChannel?.setMethodCallHandler({ [weak self]
+                (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+                if let strongSelf = self {
+                    switch(call.method) {
+                    case "incrementCounter":
+                        strongSelf.count += 1
+                        strongSelf.counterLabel.text = "Current counter: \(strongSelf.count)"
+                        strongSelf.reportCounter()
+                    case "requestCounter":
+                        strongSelf.reportCounter()
+                    default:
+                        // Unrecognized method name
+                        print("Unrecognized method name: \(call.method)")
+                    }
+                }
+            })
+        }
+    }
+
+    func reportCounter() {
+        methodChannel?.invokeMethod("reportCounter", arguments: count)
+    }
+
     @IBAction func buttonWasTapped(_ sender: Any) {
-        let flutterEngine = (UIApplication.shared.delegate as? AppDelegate)?.flutterEngine
-        let flutterViewController = FlutterViewController(engine: flutterEngine, nibName: nil, bundle: nil)!
-        self.present(flutterViewController, animated: false, completion: nil)
+        if let flutterEngine = (UIApplication.shared.delegate as? AppDelegate)?.flutterEngine {
+            let flutterViewController = FlutterViewController(engine: flutterEngine, nibName: nil, bundle: nil)
+            self.present(flutterViewController, animated: false, completion: nil)
+        }
     }
 }
-
