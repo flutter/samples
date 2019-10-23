5d4636216a5ca65b79b260ed40fda6bfc5f8ca36
Andrew Brogdon <redbrogdon@google.com>
2019-10-23 12:57:00 -0700

    Updates, working app.

--- experimental/add_to_app/android_using_prebuilt_module/app/src/main/java/dev/flutter/example/androidusingprebuiltmodule/MainActivity.kt
+++ experimental/add_to_app/android_using_prebuilt_module/app/src/main/java/dev/flutter/example/androidusingprebuiltmodule/MainActivity.kt
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package dev.flutter.example.androidfullscreen
+package dev.flutter.example.androidusingprebuiltmodule
 
 import android.os.Bundle
 import android.widget.Button
