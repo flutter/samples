3559fe0f8271a960e46756a66f4371bd9f2a0395
Andrew Brogdon <redbrogdon@google.com>
2019-12-04 12:59:03 -0800

    Created app

---
+++ experimental/add_to_app/ios_using_prebuilt_module/IOSUsingPrebuiltModule/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  IOSUsingPrebuiltModule
+//
+//  Created by Andrew Brogdon on 12/4/19.
+//  Copyright © 2019 Andrew Brogdon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func buttonWasTapped(_ sender: Any) {
+    }
+}
+
