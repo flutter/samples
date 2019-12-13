9bf9ecc6d96b2b71f323f6ccfb3dcfa13b2c7be3
Per Classon <perc@google.com>
2019-12-13 12:23:41 -0500

    Add trailing comma to improve formatting

--- gallery/gallery/lib/demos/material/snackbar_demo.dart
+++ gallery/gallery/lib/demos/material/snackbar_demo.dart
@@ -15,34 +15,35 @@ class SnackbarsDemo extends StatelessWidget {
         title: Text(GalleryLocalizations.of(context).demoSnackbarsTitle),
       ),
       body: Builder(
-          // Create an inner BuildContext so that the snackBar onPressed methods
-          // can refer to the Scaffold with Scaffold.of().
-          builder: (context) {
-        return Center(
-          child: RaisedButton(
-            child:
-                Text(GalleryLocalizations.of(context).demoSnackbarsButtonLabel),
-            onPressed: () {
-              Scaffold.of(context).showSnackBar(SnackBar(
-                content: Text(
-                  GalleryLocalizations.of(context).demoSnackbarsText,
-                ),
-                action: SnackBarAction(
-                  label: GalleryLocalizations.of(context)
-                      .demoSnackbarsActionButtonLabel,
-                  onPressed: () {
-                    Scaffold.of(context).showSnackBar(SnackBar(
-                      content: Text(
-                        GalleryLocalizations.of(context).demoSnackbarsAction,
-                      ),
-                    ));
-                  },
-                ),
-              ));
-            },
-          ),
-        );
-      }),
+        // Create an inner BuildContext so that the snackBar onPressed methods
+        // can refer to the Scaffold with Scaffold.of().
+        builder: (context) {
+          return Center(
+            child: RaisedButton(
+              child: Text(
+                  GalleryLocalizations.of(context).demoSnackbarsButtonLabel),
+              onPressed: () {
+                Scaffold.of(context).showSnackBar(SnackBar(
+                  content: Text(
+                    GalleryLocalizations.of(context).demoSnackbarsText,
+                  ),
+                  action: SnackBarAction(
+                    label: GalleryLocalizations.of(context)
+                        .demoSnackbarsActionButtonLabel,
+                    onPressed: () {
+                      Scaffold.of(context).showSnackBar(SnackBar(
+                        content: Text(
+                          GalleryLocalizations.of(context).demoSnackbarsAction,
+                        ),
+                      ));
+                    },
+                  ),
+                ));
+              },
+            ),
+          );
+        },
+      ),
     );
   }
 }
