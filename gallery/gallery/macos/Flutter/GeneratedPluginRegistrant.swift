b7b3c2c6c582e1f411a60d23b4af0dbf87f3437b
Pierre-Louis <guidezpl@users.noreply.github.com>
2019-12-19 10:41:20 +0100

    Just use url_launcher (#194)

--- gallery/gallery/macos/Flutter/GeneratedPluginRegistrant.swift
+++ gallery/gallery/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,10 @@
 import FlutterMacOS
 import Foundation
 
-import url_launcher_fde
+import shared_preferences_macos
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
