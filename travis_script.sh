b6dcc50b862dc2930efa799b4453bf803ae63022
Andrew Brogdon <redbrogdon@google.com>
2020-01-03 13:01:32 -0800

    Further tweakage

--- travis_script.sh
+++ travis_script.sh
@@ -3,13 +3,13 @@ set -e
 # Backs up one directory at a time, looking for one called "flutter". Once it
 # finds that directory, an absolute path to it is returned.
 function getFlutterPath() {
-    local path=".."
+    local path=""
     local counter=0
 
     while [[ "${counter}" -lt 10 ]]; do
-        [ -d "${path}/flutter" ] && echo "$(pwd)/${path}/flutter" && return 0
+        [ -d "${path}flutter" ] && echo "$(pwd)/${path}flutter" && return 0
         let counter++
-        path="${path}/.."
+        path="${path}../"
     done
 }
 
@@ -20,6 +20,8 @@ then
     echo "Failed to find Flutter SDK!."
     exit 1
 fi
+
+echo "Flutter SDK found at ${localSdkPath}"
 
 declare -a  PROJECT_NAMES=(
     "animations" \
