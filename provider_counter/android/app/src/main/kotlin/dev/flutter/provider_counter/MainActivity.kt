b03ae5fbd461b49e49705d597d732e30921589dc
Brett Morgan <brett.morgan@gmail.com>
2020-01-13 15:22:37 +1100

    provider_counter: update android and ios, add macos and web

--- provider_counter/android/app/src/main/kotlin/dev/flutter/provider_counter/MainActivity.kt
+++ provider_counter/android/app/src/main/kotlin/dev/flutter/provider_counter/MainActivity.kt
@@ -1,13 +1,12 @@
 package dev.flutter.provider_counter
 
-import android.os.Bundle
-
-import io.flutter.app.FlutterActivity
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
 }
