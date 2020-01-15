b27ddd136797dbde54de2bde83d2ce55aa228487
Brett Morgan <brett.morgan@gmail.com>
2020-01-16 07:25:06 +1100

    Making it actually run flutter pub upgrade

--- tool/flutter_pub_upgrade.sh
+++ tool/flutter_pub_upgrade.sh
@@ -2,27 +2,6 @@
 
 set -e
 
-# Backs up one directory at a time, looking for one called "flutter". Once it
-# finds that directory, an absolute path to it is returned.
-function getFlutterPath() {
-    local path=""
-    local counter=0
-
-    while [[ "${counter}" -lt 10 ]]; do
-        [ -d "${path}flutter" ] && echo "$(pwd)/${path}flutter" && return 0
-        let counter++
-        path="${path}../"
-    done
-}
-
-localSdkPath="$(getFlutterPath)"
-
-if [ -z "$localSdkPath" ]
-then
-    echo "Failed to find the Flutter SDK!."
-    exit 1
-fi
-
 scriptDirectory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 for file in `find "${scriptDirectory}/.." -name pubspec.yaml`
@@ -30,6 +9,6 @@ do
     (
         cd `dirname $file`
         echo "Updating `pwd`"
-        "${localSdkPath}/bin/flutter" packages get
+        flutter pub upgrade
     )
 done
