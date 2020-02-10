baed76144de3d866d44fc7096d5d25e856bbc9d7
Andrew Brogdon <redbrogdon@google.com>
2020-02-10 13:36:57 -0800

    Working tests in android_fullscreen.

--- add_to_app/flutter_module/test/widget_test.dart
+++ add_to_app/flutter_module/test/widget_test.dart
@@ -1,14 +1,10 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
+// Copyright 2019 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-
 import 'package:flutter_module/main.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
 
 class MockCounterModel extends ChangeNotifier implements CounterModel {
