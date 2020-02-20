8dc7c16f8315cc461db43e28cea4cf5e73bcee7f
John Ryan <ryjohn@google.com>
2020-02-20 10:21:49 -0800

    convert from StreamBuilder to StreamProvider

--- experimental/web_dashboard/lib/src/pages/home.dart
+++ experimental/web_dashboard/lib/src/pages/home.dart
@@ -2,48 +2,35 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../api/api.dart';
 import 'item_details.dart';
 
-class HomePage extends StatefulWidget {
-  HomePage();
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  Stream<List<Item>> _stream;
-
-  @override
+class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     var api = Provider.of<DashboardApi>(context);
-    if (_stream == null) {
-      _stream = api.items.subscribe();
-    }
 
     return Scaffold(
-      body: StreamBuilder<List<Item>>(
+      body: StreamProvider<List<Item>>(
         initialData: [],
-        stream: _stream,
-        builder: (context, snapshot) {
-          return ListView.builder(
-            itemBuilder: (context, idx) {
-              return ListTile(
-                title: Text(snapshot.data[idx].name),
-                onTap: () {
-                  _showDetails(snapshot.data[idx], context);
-                },
-              );
-            },
-            itemCount: snapshot.data.length,
-          );
-        },
+        create: (context) => api.items.subscribe(),
+        child: Consumer<List<Item>>(
+          builder: (context, items, child) {
+            return ListView.builder(
+              itemBuilder: (context, idx) {
+                return ListTile(
+                  title: Text(items[idx].name),
+                  onTap: () {
+                    _showDetails(items[idx], context);
+                  },
+                );
+              },
+              itemCount: items.length,
+            );
+          },
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
@@ -55,10 +42,8 @@ class _HomePageState extends State<HomePage> {
   }
 
   void _showDetails(Item item, BuildContext context) {
-    Navigator.of(context).push(MaterialPageRoute(
-      builder: (context) {
-        return ItemDetailsPage(item);
-      }
-    ));
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+      return ItemDetailsPage(item);
+    }));
   }
 }
