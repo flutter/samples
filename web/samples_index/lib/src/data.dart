bf0cbc6c7771c36472e3760422a3673731de63b9
John Ryan <ryjohn@google.com>
2020-03-09 15:21:26 -0700

    add and update copyright headers

--- web/samples_index/lib/src/data.dart
+++ web/samples_index/lib/src/data.dart
@@ -1,3 +1,7 @@
+// Copyright 2020 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file
+
 /// Defines the data types for this project.
 library data;
 
--- web/samples_index/lib/src/search.dart
+++ web/samples_index/lib/src/search.dart
@@ -1,3 +1,7 @@
+// Copyright 2020 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file
+
 bool matchesQuery(String query, String sampleAttributes) {
   if (query == null || query.isEmpty) {
     return true;
