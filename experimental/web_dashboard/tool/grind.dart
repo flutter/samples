64fcd453e52a5708809e34c9766ea996dad5da2b
John Ryan <ryjohn@google.com>
2020-04-07 10:54:52 -0700

    add copyright checks to grinder script

---
+++ experimental/web_dashboard/tool/grind.dart
@@ -0,0 +1,69 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:path/path.dart' as path;
+
+import 'package:grinder/grinder.dart';
+
+main(args) => grind(args);
+
+@Task()
+test() => new TestRunner().testAsync();
+
+@DefaultTask()
+@Depends(test)
+build() {
+  Pub.build();
+}
+
+@Task()
+clean() => defaultClean();
+
+const _copyright =
+    '''// Copyright 2020, the Flutter project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+''';
+
+@Task()
+copyright() async {
+  var files = <File>[];
+  await for (var file in _filesWithoutCopyright()) {
+    files.add(file);
+  }
+
+  if (files.isNotEmpty) {
+    log('Found Dart files without a copyright header:');
+    for (var file in files) log(file.toString());
+    fail('run "grind fix-copyright" to add copyright headers');
+  }
+}
+
+@Task()
+fixCopyright() async {
+  await for (var file in _filesWithoutCopyright()) {
+    var contents = await file.readAsString();
+    file.writeAsString(_copyright + '\n' + contents);
+  }
+}
+
+Stream<File> _filesWithoutCopyright() async* {
+  var set = FileSet.fromDir(Directory('.'), recurse: true);
+  var dartFiles =
+      set.files.where((file) => path.extension(file.path) == '.dart');
+
+  for (var file in dartFiles) {
+    var firstThreeLines = await file
+        .openRead()
+        .transform(utf8.decoder)
+        .transform(LineSplitter())
+        .take(3)
+        .fold('', (previous, element) {
+      if (previous == '') return element;
+      return previous + '\n' + element + '\n';
+    });
+
+    if (firstThreeLines != _copyright) {
+      yield file;
+    }
+  }
+}
