0f4d23217065bf6c92699b68624fb7ba397aca53
John Ryan <ryjohn@google.com>
2020-04-07 09:50:39 -0700

    add main_mock entrypoint

--- experimental/web_dashboard/lib/src/app.dart
+++ experimental/web_dashboard/lib/src/app.dart
@@ -2,10 +2,15 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:web_dashboard/src/api/api.dart';
+import 'package:web_dashboard/src/api/mock.dart';
+import 'package:web_dashboard/src/auth/firebase.dart';
+import 'package:web_dashboard/src/auth/mock.dart';
 
+import 'api/firebase.dart';
 import 'auth/auth.dart';
 import 'pages/home.dart';
 import 'pages/sign_in.dart';
@@ -23,13 +28,22 @@ typedef DashboardApi ApiBuilder(User user);
 
 /// An app that displays a personalized dashboard.
 class DashboardApp extends StatefulWidget {
+  static ApiBuilder _mockApiBuilder = (user) => MockDashboardApi();
+  static ApiBuilder _apiBuilder =
+      (user) => FirebaseDashboardApi(Firestore.instance, user.uid);
+
   final Auth auth;
   final ApiBuilder apiBuilder;
 
-  DashboardApp({
-    @required this.auth,
-    @required this.apiBuilder,
-  });
+  /// Runs the app using Firebase
+  DashboardApp()
+      : auth = FirebaseAuthService(),
+        apiBuilder = _apiBuilder;
+
+  /// Runs the app using mock data
+  DashboardApp.mock()
+      : auth = MockAuthService(),
+        apiBuilder = _mockApiBuilder;
 
   @override
   _DashboardAppState createState() => _DashboardAppState();
@@ -45,22 +59,21 @@ class _DashboardAppState extends State<DashboardApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        Provider<AppState>(create: (_) => _appState),
-      ],
+    return Provider.value(
+      value: _appState,
       child: MaterialApp(
-        home: SignInPage(
-          auth: _appState.auth,
-          onSuccess: (user) => _handleSignIn(user, context),
+        home: Builder(
+          builder: (context) => SignInPage(
+            auth: _appState.auth,
+            onSuccess: (user) => _handleSignIn(user, context, _appState),
+          ),
         ),
       ),
     );
   }
 
   /// Sets the DashboardApi on AppState and navigates to the home page.
-  void _handleSignIn(User user, BuildContext context) {
-    var appState = Provider.of<AppState>(context, listen: false);
+  void _handleSignIn(User user, BuildContext context, AppState appState) {
     appState.api = widget.apiBuilder(user);
 
     _showPage(HomePage(), context);
