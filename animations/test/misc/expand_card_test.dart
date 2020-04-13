02cfece74c669cb6da4b9f0bc10f97d7708385ce
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2020-04-13 10:44:33 +0530

    improves test

--- animations/test/misc/expand_card_test.dart
+++ animations/test/misc/expand_card_test.dart
@@ -11,55 +11,38 @@ Widget createExpandCardScreen() => MaterialApp(
     );
 
 void main() {
-  testWidgets('size of ExpandCard changes on tap', (tester) async {
-    await tester.pumpWidget(createExpandCardScreen());
+  group('ExpandCard Tests', () {
+    testWidgets('ExpandCard changes size on tap', (tester) async {
+      await tester.pumpWidget(createExpandCardScreen());
 
-    Size size = tester.getSize(find.byType(AnimatedContainer));
+      // Get the initial size of ExpandCard.
+      Size initialSize = tester.getSize(find.byType(ExpandCard));
 
-    // size of Expand Card is 128 initially
-    expect(size.width, 128);
+      // Tap on the ExpandCard.
+      await tester.tap(find.byType(ExpandCard));
+      await tester.pumpAndSettle();
 
-    // tap at the center of the screen
-    await tester.tapAt(const Offset(400.0, 300.0));
-    await tester.pumpAndSettle(Duration(milliseconds: 300));
+      // The size of ExpandCard must change once tapped.
+      expect(
+        initialSize,
+        isNot(equals(tester.getSize(find.byType(ExpandCard)))),
+      );
+    });
 
-    size = tester.getSize(find.byType(AnimatedContainer));
-    // size of Expand Card is 256, when card is selected
-    expect(size.width, 256);
-  });
+    testWidgets('ExpandCard changes image on tap', (tester) async {
+      await tester.pumpWidget(createExpandCardScreen());
 
-  testWidgets('assets images are loaded properly', (tester) async {
-    await tester.pumpWidget(createExpandCardScreen());
+      Image initialImage = tester.widget(find.byType(Image).last);
 
-    AnimatedCrossFade animatedCrossFade =
-        tester.widget(find.byType(AnimatedCrossFade));
+      // Tap on ExpandCard.
+      await tester.tap(find.byType(ExpandCard));
+      await tester.pumpAndSettle();
 
-    Image firstImage = animatedCrossFade.firstChild as Image;
-    Image secondImage = animatedCrossFade.secondChild as Image;
-
-    AssetImage assetImage = firstImage.image as AssetImage;
-    // The first widget image is is 'assets/eat_cape_town_sm.jpg'
-    expect(assetImage.assetName, 'assets/eat_cape_town_sm.jpg');
-
-    assetImage = secondImage.image as AssetImage;
-    // The second widget image is 'assets/eat_new_orleans_sm.jpg'
-    expect(assetImage.assetName, 'assets/eat_new_orleans_sm.jpg');
-  });
-
-  testWidgets('ExpandCard changes image on tap', (tester) async {
-    await tester.pumpWidget(createExpandCardScreen());
-
-    AnimatedCrossFade animatedCrossFade =
-        tester.widget(find.byType(AnimatedCrossFade));
-    // initially the first child is shown
-    expect(animatedCrossFade.crossFadeState.index, 0);
-
-    // tap at the center of the screen
-    await tester.tapAt(const Offset(400.0, 300.0));
-    await tester.pumpAndSettle(Duration(milliseconds: 300));
-
-    animatedCrossFade = tester.widget(find.byType(AnimatedCrossFade));
-    //shows the second child when ExpandCard is tapped
-    expect(animatedCrossFade.crossFadeState.index, 1);
+      // Once tapped, the image should change.
+      expect(
+        initialImage,
+        isNot(equals(tester.widget(find.byType(Image).last))),
+      );
+    });
   });
 }
