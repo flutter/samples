3d8687e725d2380604140ff6cabac50bbc9e191a
John Ryan <ryjohn@google.com>
2020-04-28 15:58:46 -0700

    add license headers

--- web/slide_puzzle/lib/src/widgets/decoration_image_plus.dart
+++ web/slide_puzzle/lib/src/widgets/decoration_image_plus.dart
@@ -1,3 +1,7 @@
+// Copyright 2020, the Flutter project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 // ignore_for_file: omit_local_variable_types, annotate_overrides
 
 import 'dart:developer' as developer;
