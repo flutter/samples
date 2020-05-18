261842e31b7b9e207a164f3b998821096bec2087
John Ryan <ryjohn@google.com>
2020-05-18 14:00:37 -0700

    remove unused import

--- experimental/web_dashboard/lib/src/pages/entries.dart
+++ experimental/web_dashboard/lib/src/pages/entries.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:web_dashboard/src/api/api.dart';
