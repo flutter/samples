d1251b49cf80aa623bce8ad5dada0e494cb91508
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2020-05-19 11:03:21 +0530

    adds license

--- animations/test/misc/carousel_test.dart
+++ animations/test/misc/carousel_test.dart
@@ -1,3 +1,7 @@
+// Copyright 2020 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:animations/src/misc/carousel.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
