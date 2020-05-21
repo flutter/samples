ad7795362565e888c07325ecb1e48bca851bbf68
John Ryan <ryjohn@google.com>
2020-05-21 09:24:04 -0700

    add copyright headers

--- experimental/web_dashboard/lib/src/utils/day_helpers.dart
+++ experimental/web_dashboard/lib/src/utils/day_helpers.dart
@@ -1,3 +1,7 @@
+// Copyright 2020, the Flutter project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 extension DayUtils on DateTime {
   /// The UTC date portion of a datetime, without the minutes, seconds, etc.
   DateTime get atMidnight {
