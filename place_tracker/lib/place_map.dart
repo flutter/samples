3eb006e76fe0eb5988b0eaef5e5c79e48e9d4dd3
Tushar Ojha <tusharojha2001@gmail.com>
2020-06-06 13:46:57 +0530

    formatted import statements

--- place_tracker/lib/place_map.dart
+++ place_tracker/lib/place_map.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
