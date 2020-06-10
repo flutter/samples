flutter/samples

commit 14b9a8c4c41334c93483c33f07b6e8334775a24e

Flutter 1.17 updates (#432)
* Material text theme updates & flutter pub upgrade (#411)

* Beta channel updates (#412)

* fix deprecated usages in provider_shopper - beta channel

* fix deprecated usages in animations - beta channel

* fix deprecated usages in provider_counter - beta

* Updates additional samples for beta release (#416)

* use headline6 instead of title

* fix another title -> headline6

Co-authored-by: Brett Morgan <brett.morgan@gmail.com>
Co-authored-by: Andrew Brogdon <RedBrogdon@users.noreply.github.com>

provider_shopper/lib/common/theme.dart
@@ -7,7 +7,7 @@ import 'package:flutter/material.dart';
 final appTheme = ThemeData(
   primarySwatch: Colors.yellow,
   textTheme: TextTheme(
-    display4: TextStyle(
+    headline2: TextStyle(
       fontFamily: 'Corben',
       fontWeight: FontWeight.w700,
       fontSize: 24,

.travis.yml
@@ -78,6 +78,7 @@ notifications:
 branches:
   only:
   - master
+  - beta
 
 before_install:
 # Decrypt credentials for Firebase Test Lab service account. This key will only

add_to_app/flutter_module/lib/main.dart
@@ -124,14 +124,14 @@ class Contents extends StatelessWidget {
                 Text(
                   'Window is ${mediaInfo.size.width.toStringAsFixed(1)} x '
                   '${mediaInfo.size.height.toStringAsFixed(1)}',
-                  style: Theme.of(context).textTheme.headline,
+                  style: Theme.of(context).textTheme.headline5,
                 ),
                 SizedBox(height: 16),
                 Consumer<CounterModel>(
                   builder: (context, model, child) {
                     return Text(
                       'Taps: ${model.count}',
-                      style: Theme.of(context).textTheme.headline,
+                      style: Theme.of(context).textTheme.headline5,
                     );
                   },
                 ),

add_to_app/flutter_module_using_plugin/lib/main.dart
@@ -126,14 +126,14 @@ class Contents extends StatelessWidget {
                 Text(
                   'Window is ${mediaInfo.size.width.toStringAsFixed(1)} x '
                   '${mediaInfo.size.height.toStringAsFixed(1)}',
-                  style: Theme.of(context).textTheme.headline,
+                  style: Theme.of(context).textTheme.headline5,
                 ),
                 SizedBox(height: 16),
                 Consumer<CounterModel>(
                   builder: (context, model, child) {
                     return Text(
                       'Taps: ${model.count}',
-                      style: Theme.of(context).textTheme.headline,
+                      style: Theme.of(context).textTheme.headline5,
                     );
                   },
                 ),

animations/lib/main.dart
@@ -142,7 +142,7 @@ class AnimationSamples extends StatelessWidget {
 
 class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
-    final headerStyle = Theme.of(context).textTheme.title;
+    final headerStyle = Theme.of(context).textTheme.headline6;
     return Scaffold(
       appBar: AppBar(
         title: Text('Animation Samples'),

animations/lib/src/basics/02_page_route_builder.dart
@@ -42,7 +42,7 @@ class _Page2 extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(),
       body: Center(
-        child: Text('Page 2!', style: Theme.of(context).textTheme.display1),
+        child: Text('Page 2!', style: Theme.of(context).textTheme.headline4),
       ),
     );
   }

animations/lib/src/basics/03_animation_controller.dart
@@ -62,7 +62,7 @@ class _AnimationControllerDemoState extends State<AnimationControllerDemo>
               constraints: BoxConstraints(maxWidth: 200),
               child: Text(
                 '${controller.value.toStringAsFixed(2)}',
-                style: Theme.of(context).textTheme.display2,
+                style: Theme.of(context).textTheme.headline3,
                 textScaleFactor: 1 + controller.value,
               ),
             ),

animations/lib/src/misc/curved_animation.dart
@@ -92,7 +92,7 @@ class _CurvedAnimationDemoState extends State<CurvedAnimationDemo>
           SizedBox(height: 20.0),
           Text(
             'Select Curve for forward motion',
-            style: Theme.of(context).textTheme.title,
+            style: Theme.of(context).textTheme.headline6,
           ),
           DropdownButton<CurveChoice>(
             items: curves.map((curve) {
@@ -110,7 +110,7 @@ class _CurvedAnimationDemoState extends State<CurvedAnimationDemo>
           SizedBox(height: 15.0),
           Text(
             'Select Curve for reverse motion',
-            style: Theme.of(context).textTheme.title,
+            style: Theme.of(context).textTheme.headline6,
           ),
           DropdownButton<CurveChoice>(
             items: curves.map((curve) {

commit 30ee7202e26637896d8eccde9e93c96c4d33a853

Code of other files depends on `headline1` (#466)
Code of other files depends on `headline1`, but there's only `headline2` defined in the **theme.dart** file.

This was a regression introduced with #432.

provider_shopper/lib/common/theme.dart
@@ -7,7 +7,7 @@ import 'package:flutter/material.dart';
 final appTheme = ThemeData(
   primarySwatch: Colors.yellow,
   textTheme: TextTheme(
-    headline2: TextStyle(
+    headline1: TextStyle(
       fontFamily: 'Corben',
       fontWeight: FontWeight.w700,
       fontSize: 24,