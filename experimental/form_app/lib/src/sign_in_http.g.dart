ecb45ce3f8ae48eefe07c862c788a9fcc4dc4d3c
John Ryan <ryjohn@google.com>
2020-06-12 11:45:15 -0700

    run package:copywriter

--- experimental/form_app/lib/src/sign_in_http.g.dart
+++ experimental/form_app/lib/src/sign_in_http.g.dart
@@ -1,3 +1,7 @@
+// Copyright 2020, the Flutter project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
 part of 'sign_in_http.dart';
