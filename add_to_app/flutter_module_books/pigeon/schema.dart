70976eeb28bb1b37df49ce841f128fad80bf8ad6
xster <xster@google.com>
2020-06-12 23:18:05 -0700

    Add a pigeon sample that demonstrates a "realistic" integration scenario with middleware and business logic (#465)

---
+++ add_to_app/flutter_module_books/pigeon/schema.dart
@@ -0,0 +1,32 @@
+// Copyright 2020 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:pigeon/pigeon.dart';
+
+class Book {
+  String title;
+  String subtitle;
+  String author;
+  String description;
+  String publishDate;
+  int pageCount;
+  // Thumbnail thumbnail;
+}
+
+// TODO(gaaclarke): add this back when the https://github.com/flutter/flutter/issues/58896
+// crash is resolved.
+// class Thumbnail {
+//   String url;
+// }
+
+@FlutterApi()
+abstract class FlutterBookApi {
+  void displayBookDetails(Book book);
+}
+
+@HostApi()
+abstract class HostBookApi {
+  void cancel();
+  void finishEditingBook(Book book);
+}
---
+++ add_to_app/flutter_module_books/test/widget_test.dart
@@ -0,0 +1,43 @@
+// Copyright 2020 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_module_books/api.dart';
+import 'package:flutter_module_books/main.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+void main() {
+  testWidgets('Pressing clear calls the cancel API',
+      (WidgetTester tester) async {
+    MockHostBookApi mockHostApi = MockHostBookApi();
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: BookDetail(hostApi: mockHostApi),
+      ),
+    );
+
+    await tester.tap(find.byIcon(Icons.clear));
+
+    verify(mockHostApi.cancel());
+  });
+
+  testWidgets('Pressing done calls the finish editing API',
+      (WidgetTester tester) async {
+    MockHostBookApi mockHostApi = MockHostBookApi();
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: BookDetail(hostApi: mockHostApi),
+      ),
+    );
+
+    await tester.tap(find.byIcon(Icons.check));
+
+    verify(mockHostApi.finishEditingBook(any));
+  });
+}
+
+class MockHostBookApi extends Mock implements HostBookApi {}
