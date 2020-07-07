36f1317dad21a52406d2a774393dc65e89326622
Brett Morgan <brett.morgan@gmail.com>
2020-07-08 09:11:28 +1000

    Update linux compilation generated files

--- experimental/desktop_photo_search/linux/flutter/generated_plugin_registrant.cc
+++ experimental/desktop_photo_search/linux/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,7 @@
 
 #include <file_chooser/file_chooser_plugin.h>
 #include <menubar/menubar_plugin.h>
+#include <url_launcher_linux/url_launcher_plugin.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
   g_autoptr(FlPluginRegistrar) file_chooser_registrar =
@@ -14,4 +15,7 @@ void fl_register_plugins(FlPluginRegistry* registry) {
   g_autoptr(FlPluginRegistrar) menubar_registrar =
       fl_plugin_registry_get_registrar_for_plugin(registry, "MenubarPlugin");
   menubar_plugin_register_with_registrar(menubar_registrar);
+  g_autoptr(FlPluginRegistrar) url_launcher_linux_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "UrlLauncherPlugin");
+  url_launcher_plugin_register_with_registrar(url_launcher_linux_registrar);
 }
--- experimental/desktop_photo_search/linux/flutter/generated_plugins.cmake
+++ experimental/desktop_photo_search/linux/flutter/generated_plugins.cmake
@@ -5,6 +5,7 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   file_chooser
   menubar
+  url_launcher_linux
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
