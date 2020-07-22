47ca7dae8dcccab41a6d21db35f9758278c81cfb
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2020-07-23 00:32:58 +0530

    adds test for platform interface

--- experimental/federated_plugin/federated_plugin_platform_interface/test/federated_plugin_platform_interface_test.dart
+++ experimental/federated_plugin/federated_plugin_platform_interface/test/federated_plugin_platform_interface_test.dart
@@ -1,3 +1,24 @@
+import 'package:federated_plugin_platform_interface/location_method_channel.dart';
+import 'package:federated_plugin_platform_interface/location_model.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  group('MethodChannel test', () {
+    final location = Location(latitude: 1.0, longitude: 2.0);
+    MethodChannel('location').setMockMethodCallHandler((call) async {
+      if (call.method == 'getLocation') {
+        return [location.longitude, location.latitude];
+      }
+    });
+
+    test('getLocation method test', () async {
+      final locationMethodChannel = LocationMethodChannel();
+      final result = await locationMethodChannel.getLocation();
+      expect(result.longitude, location.longitude);
+      expect(result.latitude, result.latitude);
+    });
+  });
 }
