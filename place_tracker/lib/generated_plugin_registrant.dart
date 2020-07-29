31d64e3e1430de4c322b7918797dfe11f4fbc9db
John Ryan <ryjohn@google.com>
2020-07-29 14:58:18 -0700

    add generated_plugin_registrant.dart

---
+++ place_tracker/lib/generated_plugin_registrant.dart
@@ -0,0 +1,16 @@
+//
+// Generated file. Do not edit.
+//
+
+// ignore: unused_import
+import 'dart:ui';
+
+import 'package:google_maps_flutter_web/google_maps_flutter_web.dart';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+// ignore: public_member_api_docs
+void registerPlugins(PluginRegistry registry) {
+  GoogleMapsPlugin.registerWith(registry.registrarFor(GoogleMapsPlugin));
+  registry.registerMessageHandler();
+}
