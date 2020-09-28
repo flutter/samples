107a3827467dc0cefe3df0d9f0e7ae2ab0b52ba8
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2020-09-28 10:45:20 +0530

    intial commit for using upower

--- experimental/federated_plugin/federated_plugin_linux/lib/federated_plugin_linux.dart
+++ experimental/federated_plugin/federated_plugin_linux/lib/federated_plugin_linux.dart
@@ -1,3 +1,22 @@
-// The federated_plugin_linux uses the default BatteryMethodChannel used by
-// federated_plugin_platform_interface to do platform calls.
+import 'package:dbus/dbus.dart';
+import 'package:federated_plugin_platform_interface/federated_plugin_platform_interface.dart';
+import 'package:upower/upower.dart';
 
+class FederatedPluginLinux extends FederatedPluginInterface {
+
+  static void register() {
+    FederatedPluginInterface.instance = FederatedPluginLinux();
+  }
+
+  @override
+  Future<int> getBatteryLevel() async {
+    var uPowerClient = UPowerClient(DBusClient.system());
+
+    double batteryLevelSum;
+    uPowerClient.devices.forEach((device) {
+      batteryLevelSum += device.percentage;
+    });
+
+    return (batteryLevelSum / uPowerClient.devices.length).round();
+  }
+}
---
+++ experimental/federated_plugin/federated_plugin_linux/test/federated_plugin_linux_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:federated_plugin_linux/federated_plugin_linux.dart';
+
+void main() {
+  test('adds one to input values', () {
+  });
+}
