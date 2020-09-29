ef3a8a20b561c41ddef18fee88ba12680b9d2610
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2020-09-29 10:48:12 +0530

    adds test for linux implementation

--- experimental/federated_plugin/federated_plugin_linux/lib/federated_plugin_linux.dart
+++ experimental/federated_plugin/federated_plugin_linux/lib/federated_plugin_linux.dart
@@ -1,11 +1,21 @@
+// Copyright 2020 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:dbus/dbus.dart';
 import 'package:federated_plugin_platform_interface/federated_plugin_platform_interface.dart';
 import 'package:upower/upower.dart';
 
+/// Linux Implementation of [FederatedPluginInterface] to retrieve current battery
+/// level of device.
 class FederatedPluginLinux extends FederatedPluginInterface {
+  FederatedPluginLinux({this.uPowerClientMock});
+
+  UPowerClient uPowerClientMock;
+
   @override
   Future<int> getBatteryLevel() async {
-    var uPowerClient = UPowerClient(DBusClient.system());
+    var uPowerClient = uPowerClientMock ?? UPowerClient(DBusClient.system());
     await uPowerClient.connect();
 
     var batteryLevelSum = 0.0;
--- experimental/federated_plugin/federated_plugin_linux/test/federated_plugin_linux_test.dart
+++ experimental/federated_plugin/federated_plugin_linux/test/federated_plugin_linux_test.dart
@@ -1,8 +1,46 @@
-import 'package:flutter_test/flutter_test.dart';
+// Copyright 2020 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 import 'package:federated_plugin_linux/federated_plugin_linux.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:upower/upower.dart';
+
+double kBatteryLevel = 45.0;
 
 void main() {
-  test('adds one to input values', () {
+  group('FederatedPluginLinux tests', () {
+    test('getBatteryLevel test', () async {
+      var batteryLevel =
+          await FederatedPluginLinux(uPowerClientMock: UPowerClientMock())
+              .getBatteryLevel();
+
+      // Verify the battery level.
+      expect(batteryLevel, kBatteryLevel);
+    });
   });
+}
+
+class UPowerClientMock extends Mock implements UPowerClient {
+  @override
+  Iterable<UPowerDevice> get devices => [UPowerDeviceMock()];
+
+  @override
+  Future connect() async {
+    return;
+  }
+
+  @override
+  void close() {
+    return;
+  }
+}
+
+class UPowerDeviceMock extends Mock implements UPowerDevice {
+  @override
+  double get percentage => kBatteryLevel;
+
+  @override
+  UPowerDeviceType get type => UPowerDeviceType.battery;
 }
