c57122090296bffd63894c4ae4093f6b47141e47
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2020-10-10 20:45:30 +0530

    add ios implementation for ios
    reset the author

--- platform_channels/ios/Runner/AppDelegate.swift
+++ platform_channels/ios/Runner/AppDelegate.swift
@@ -3,11 +3,31 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        let flutterViewController = window.rootViewController as! FlutterViewController
+        FlutterMethodChannel(name: "methodChannelDemo", binaryMessenger: flutterViewController.binaryMessenger).setMethodCallHandler({
+            (call: FlutterMethodCall, result: FlutterResult) -> Void in
+            
+            let count = (call.arguments as? NSDictionary)?["count"] as? Int
+            
+            if count == nil {
+                result(FlutterError(code: "INVALID_ARGUMENT", message: "Value of count cannot be null", details: nil))
+            } else {
+                switch call.method {
+                case "increment":
+                    result(count! + 1)
+                case "decrement":
+                    result(count! - 1)
+                default:
+                    result(FlutterMethodNotImplemented)
+                }
+            }
+        })
+        
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
 }
