0c011574d015777b093fa1413ef70261310342bc
Ayush Bherwani <ayush.bherwani1998@gmail.com>
2020-11-03 03:45:00 +0530

    add iOS implementation for battery plugin (#579)

--- experimental/federated_plugin/federated_plugin/ios/Classes/SwiftFederatedPlugin.swift
+++ experimental/federated_plugin/federated_plugin/ios/Classes/SwiftFederatedPlugin.swift
@@ -3,12 +3,24 @@ import UIKit
 
 public class SwiftFederatedPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "federated_plugin", binaryMessenger: registrar.messenger())
+    let channel = FlutterMethodChannel(name: "battery", binaryMessenger: registrar.messenger())
     let instance = SwiftFederatedPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    guard  call.method == "getBatteryLevel"  else {
+        result(FlutterMethodNotImplemented)
+        return
+    }
+    
+    let device = UIDevice.current
+    device.isBatteryMonitoringEnabled = true
+    
+    if device.batteryState == UIDevice.BatteryState.unknown {
+        result(FlutterError(code: "STATUS_UNAVAILABLE", message: "Not able to determine battery level", details: nil))
+    }
+    
+    result(Int(device.batteryLevel * 100))
   }
 }
