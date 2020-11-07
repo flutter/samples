25bfc7846db5e7a6cd9f04f0cc6d1889bfbb9f0d
Brett Morgan <brettmorgan@google.com>
2020-11-07 12:23:48 +1100

    Re-generate built_value classes (#587)

--- experimental/desktop_photo_search/lib/src/model/search.g.dart
+++ experimental/desktop_photo_search/lib/src/model/search.g.dart
@@ -53,7 +53,7 @@ class _$SearchSerializer implements StructuredSerializer<Search> {
           result.results.replace(serializers.deserialize(value,
                   specifiedType:
                       const FullType(BuiltList, const [const FullType(Photo)]))
-              as BuiltList<dynamic>);
+              as BuiltList<Object>);
           break;
       }
     }
