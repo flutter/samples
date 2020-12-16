6f2647d1ae6ff659cf937a6cb79f8e38c0e74896
Andrew Brogdon <redbrogdon@google.com>
2020-12-16 12:24:45 -0800

    Adding Android

--- tool/travis_android_script.sh
+++ tool/travis_android_script.sh
@@ -2,29 +2,6 @@
 
 set -e
 
-# Backs up one directory at a time, looking for one called "flutter". Once it
-# finds that directory, an absolute path to it is returned.
-function getFlutterPath() {
-    local path=""
-    local counter=0
-
-    while [[ "${counter}" -lt 10 ]]; do
-        [ -d "${path}flutter" ] && echo "$(pwd)/${path}flutter" && return 0
-        let counter++
-        path="${path}../"
-    done
-}
-
-readonly LOCAL_SDK_PATH=$(getFlutterPath)
-
-if [ -z "${LOCAL_SDK_PATH}" ]
-then
-    echo "Failed to find the Flutter SDK!"
-    exit 1
-fi
-
-echo "Flutter SDK found at ${LOCAL_SDK_PATH}"
-
 echo "Fetching dependencies and building 'flutter_module'."
 pushd add_to_app/flutter_module
 "${LOCAL_SDK_PATH}/bin/flutter" packages get
@@ -59,6 +36,7 @@ done
 if [ ! -f "svc-keyfile.json" ]
 then
     echo "Keyfile for Firebase Test Lab not found. Skipping integration tests."
+    echo "-- Success --"
     exit 0
 fi
 
