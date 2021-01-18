fa1b48d50fc2ae089f7e0eba9c058b57aafd6896
Brett Morgan <brett.morgan@gmail.com>
2021-01-18 13:11:09 +1100

    Split CI testing based on flutter channels

---
+++ tool/flutter_ci_script_stable.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+set -e
+
+declare -ar PROJECT_NAMES=(
+    "add_to_app/flutter_module" \
+    "add_to_app/flutter_module_using_plugin" \
+    "add_to_app/flutter_module_books" \
+    "animations" \
+    "flutter_maps_firestore" \
+    "infinite_list" \
+    "ios_app_clip" \
+    "isolate_example" \
+    "jsonexample" \
+    "place_tracker" \
+    "platform_channels" \
+    "platform_design"
+    "platform_view_swift" \
+    "provider_counter" \
+    "provider_shopper" \
+    "testing_app" \
+# TODO(goderbauer): Add veggieseasons back when moved out of experimental again.
+#    "veggieseasons" \
+)
+
+for PROJECT_NAME in "${PROJECT_NAMES[@]}"
+do
+    echo "== Testing '${PROJECT_NAME}' on Flutter's ${FLUTTER_VERSION} channel =="
+    pushd "${PROJECT_NAME}"
+
+    # Grab packages.
+    flutter pub get
+
+    # Run the analyzer to find any static analysis issues.
+    flutter analyze
+
+    # Reformat the web plugin registrant, if necessary.
+    if [ -f "lib/generated_plugin_registrant.dart" ]
+    then
+        echo "Renaming $(pwd)/lib/generated_plugin_registrant.dart"
+        flutter format "lib/generated_plugin_registrant.dart"
+    fi
+
+    # Run the formatter on all the dart files to make sure everything's linted.
+    flutter format -n --set-exit-if-changed .
+
+    # Run the actual tests.
+    flutter test
+
+    popd
+done
+
+echo "-- Success --"
