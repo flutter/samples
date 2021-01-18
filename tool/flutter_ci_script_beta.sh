f1e344ffb87180dbf4d2cdc43a5b57128a15bbb6
Brett Morgan <brett.morgan@gmail.com>
2021-01-18 15:44:44 +1100

    Drop experimental/web_dashboard from CI

--- tool/flutter_ci_script_beta.sh
+++ tool/flutter_ci_script_beta.sh
@@ -30,7 +30,8 @@ declare -ar PROJECT_NAMES=(
     "experimental/null_safety/null_safe_app" \
     "experimental/null_safety/null_unsafe_app" \
     "experimental/veggieseasons" \
-    "experimental/web_dashboard" \
+    # Tracking issue: https://github.com/flutter/samples/issues/660
+    # "experimental/web_dashboard" \
 )
 
 for PROJECT_NAME in "${PROJECT_NAMES[@]}"
