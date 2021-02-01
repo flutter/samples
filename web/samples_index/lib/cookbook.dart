b395e8031cb095b171e78cfb15dc90c53de6a9f5
John Ryan <ryjohn@google.com>
2021-02-01 10:32:38 -0800

    add category to image name to avoid conflicts
    e.g.
    testing/integration/introduction
    testing/widget/introduction

--- web/samples_index/lib/cookbook.dart
+++ web/samples_index/lib/cookbook.dart
@@ -72,5 +72,9 @@ String screenshotPath(String url) {
 String parseFileName(String link) {
   var p = path.basename(link);
   var dot = p.indexOf('.');
-  return p.substring(0, dot);
+  var detailName = p.substring(0, dot);
+  // var categoryName = path.split(link);
+  var components = path.split(link);
+  var categoryName = components[components.length - 2];
+  return '$categoryName-$detailName';
 }
