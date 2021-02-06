a36dc006989c5407fa38e8008040e28a7405c45f
Adarsh kumar singh <47661086+adarsh-technocrat@users.noreply.github.com>
2021-02-06 06:47:23 +0530

    Updated deprecated widgets with new one (#666)

--- testing_app/lib/screens/favorites.dart
+++ testing_app/lib/screens/favorites.dart
@@ -55,7 +55,7 @@ class FavoriteItemTile extends StatelessWidget {
           icon: Icon(Icons.close),
           onPressed: () {
             Provider.of<Favorites>(context, listen: false).remove(itemNo);
-            Scaffold.of(context).showSnackBar(
+            ScaffoldMessenger.of(context).showSnackBar(
               SnackBar(
                 content: Text('Removed from favorites.'),
                 duration: Duration(seconds: 1),
