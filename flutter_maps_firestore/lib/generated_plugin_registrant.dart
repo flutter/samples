8155ce322299607633c5045851f64326ba83ad81
Andrew Brogdon <redbrogdon@google.com>
2021-02-06 17:13:45 -0800

    Updated for beta branch.

--- flutter_maps_firestore/lib/generated_plugin_registrant.dart
+++ flutter_maps_firestore/lib/generated_plugin_registrant.dart
@@ -2,14 +2,18 @@
 // Generated file. Do not edit.
 //
 
-// ignore: unused_import
-import 'dart:ui';
+// ignore_for_file: lines_longer_than_80_chars
 
+import 'package:cloud_firestore_web/cloud_firestore_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:location_web/location_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
-void registerPlugins(PluginRegistry registry) {
-  FirebaseCoreWeb.registerWith(registry.registrarFor(FirebaseCoreWeb));
-  registry.registerMessageHandler();
+// ignore: public_member_api_docs
+void registerPlugins(Registrar registrar) {
+  FirebaseFirestoreWeb.registerWith(registrar);
+  FirebaseCoreWeb.registerWith(registrar);
+  LocationWebPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
 }
