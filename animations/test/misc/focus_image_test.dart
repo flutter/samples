a8e4c149330356df4d60d27aba9371c38c59e351
Arteev Raina <arteevraina@gmail.com>
2021-02-09 14:03:33 +0530

    [animations] added test for focus image

---
+++ animations/test/misc/focus_image_test.dart
@@ -0,0 +1,56 @@
+// Copyright 2019 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:animations/src/misc/focus_image.dart';
+
+Widget createFocusImageScreen() => MaterialApp(
+      home: FocusImageDemo(),
+    );
+
+void main() {
+  group('FocusImage Tests', () {
+    testWidgets('Size of ink well changes on tap', (tester) async {
+      await tester.pumpWidget(createFocusImageScreen());
+
+      // Get the initial inkwell.
+      final initialInkwell = tester.firstWidget(find.byType(InkWell));
+
+      // Get the size of initial inkwell.
+      var initialSize = tester.getSize(find.byWidget(initialInkwell));
+
+      // Tap on the ink well at index 0.
+      await tester.tap(find.byType(InkWell).at(0));
+      await tester.pumpAndSettle();
+
+      // Get the final inkwell.
+      final finalInkwell = tester.firstWidget(find.byType(InkWell));
+
+      // Get the size of final inkwell.
+      var finalSize = tester.getSize(find.byWidget(finalInkwell));
+
+      // Final size should be greater than initial size.
+      expect(
+        finalSize,
+        greaterThan(initialSize),
+      );
+    });
+
+    testWidgets('Final inkwell on tap goes back to the grid', (tester) async {
+      await tester.pumpWidget(createFocusImageScreen());
+
+      // Tap on the ink well at index 0.
+      await tester.tap(find.byType(InkWell).at(0));
+      await tester.pumpAndSettle();
+
+      // Tap on the final inkwell.
+      await tester.tap(find.byType(InkWell));
+      await tester.pumpAndSettle();
+
+      // The grid view should appear again.
+      expect(find.byType(Grid), findsOneWidget);
+    });
+  });
+}
