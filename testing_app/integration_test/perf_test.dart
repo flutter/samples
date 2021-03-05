ed965384d3d71a5993fe4dd0c2d86bc5979fa586
“abd99” <abdullahzakir99@gmail.com>
2021-03-06 02:53:36 +0530

    Remove use of traceAction

--- testing_app/integration_test/perf_test.dart
+++ testing_app/integration_test/perf_test.dart
@@ -23,96 +23,78 @@ void main() {
       final listFinder = find.byType(ListView);
       final scroller = tester.widget<ListView>(listFinder).controller;
 
-      // Record the performance timeline of the following operations.
-      await binding.traceAction(
+      // Record the performance summary as the app scrolls through
+      // the list of items.
+      await binding.watchPerformance(
         () async {
-          // Record the performance summary as the app scrolls through
-          // the list of items.
-          await binding.watchPerformance(
-            () async {
-              // Quickly scroll all the way down.
-              await scroller.animateTo(
-                7000,
-                duration: const Duration(seconds: 1),
-                curve: Curves.linear,
-              );
-              await tester.pumpAndSettle();
-
-              // Quickly scroll back up all the way.
-              await scroller.animateTo(
-                -7000,
-                duration: const Duration(seconds: 1),
-                curve: Curves.linear,
-              );
-              await tester.pumpAndSettle();
-            },
-            // Send the performance summary to the driver.
-            reportKey: 'scrolling_summary',
+          // Quickly scroll all the way down.
+          await scroller.animateTo(
+            7000,
+            duration: const Duration(seconds: 1),
+            curve: Curves.linear,
           );
+          await tester.pumpAndSettle();
+
+          // Quickly scroll back up all the way.
+          await scroller.animateTo(
+            -7000,
+            duration: const Duration(seconds: 1),
+            curve: Curves.linear,
+          );
+          await tester.pumpAndSettle();
         },
-        // Send the timeline data to the driver.
-        // This timeline can be opened in the Chrome browser's tracing tools
-        // by navigating to chrome://tracing.
-        reportKey: 'scrolling_timeline',
+        // Send the performance summary to the driver.
+        reportKey: 'scrolling_summary',
       );
     });
 
     testWidgets('Favorites operations test', (tester) async {
       await tester.pumpWidget(TestingApp());
 
-      // Record the performance timeline of the following operations.
-      await binding.traceAction(
+      // Record the performance summary as operations are performed
+      // on the favorites list.
+      await binding.watchPerformance(
         () async {
-          // Record the performance summary as operations are performed
-          // on the favorites list.
-          await binding.watchPerformance(
-            () async {
-              // Create a list of icon keys.
-              final iconKeys = [
-                'icon_0',
-                'icon_1',
-                'icon_2',
-              ];
+          // Create a list of icon keys.
+          final iconKeys = [
+            'icon_0',
+            'icon_1',
+            'icon_2',
+          ];
 
-              // Add first three items to favorites.
-              for (var icon in iconKeys) {
-                // Tap onto the icon.
-                await tester.tap(find.byKey(ValueKey(icon)));
-                await tester.pumpAndSettle();
+          // Add first three items to favorites.
+          for (var icon in iconKeys) {
+            // Tap onto the icon.
+            await tester.tap(find.byKey(ValueKey(icon)));
+            await tester.pumpAndSettle();
 
-                // Verify if appropriate message appears.
-                expect(find.text('Added to favorites.'), findsOneWidget);
-              }
+            // Verify if appropriate message appears.
+            expect(find.text('Added to favorites.'), findsOneWidget);
+          }
 
-              // Tap onto the favorites button on the AppBar.
-              // The Favorites List should appear.
-              await tester.tap(find.text('Favorites'));
-              await tester.pumpAndSettle();
+          // Tap onto the favorites button on the AppBar.
+          // The Favorites List should appear.
+          await tester.tap(find.text('Favorites'));
+          await tester.pumpAndSettle();
 
-              final removeIconKeys = [
-                'remove_icon_0',
-                'remove_icon_1',
-                'remove_icon_2',
-              ];
+          final removeIconKeys = [
+            'remove_icon_0',
+            'remove_icon_1',
+            'remove_icon_2',
+          ];
 
-              // Remove all the items from favorites.
-              for (final iconKey in removeIconKeys) {
-                // Tap onto the remove icon.
-                await tester.tap(find.byKey(ValueKey(iconKey)));
-                await tester.pumpAndSettle();
+          // Remove all the items from favorites.
+          for (final iconKey in removeIconKeys) {
+            // Tap onto the remove icon.
+            await tester.tap(find.byKey(ValueKey(iconKey)));
+            await tester.pumpAndSettle();
 
-                // Verify if appropriate message appears.
-                expect(find.text('Removed from favorites.'), findsOneWidget);
-              }
-            },
-            // Send the performance summary to the driver.
-            reportKey: 'favorites_operations_summary',
-          );
+            // Verify if appropriate message appears.
+            expect(find.text('Removed from favorites.'), findsOneWidget);
+          }
         },
-        // Send the timeline data to the driver.
-        // This timeline can be opened in the Chrome browser's tracing tools
-        // by navigating to chrome://tracing.
-        reportKey: 'favorites_operations_timeline',
+        // Send the performance summary to the driver.
+        reportKey: 'favorites_operations_summary',
       );
     });
   });
