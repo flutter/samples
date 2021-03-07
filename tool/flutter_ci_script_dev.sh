645e579980e28e6fb4e52274663cbce654c06345
Brett Morgan <brettmorgan@google.com>
2021-03-08 09:01:35 +1100

    Re-enable all apps on dev channel

--- tool/flutter_ci_script_dev.sh
+++ tool/flutter_ci_script_dev.sh
@@ -3,37 +3,30 @@
 set -e
 
 declare -ar PROJECT_NAMES=(
-    "add_to_app/fullscreen/flutter_module" \
-    "add_to_app/prebuilt_module/flutter_module" \
-    "add_to_app/plugin/flutter_module_using_plugin" \
-    "add_to_app/books/flutter_module_books" \
-    "add_to_app/multiple_flutters/multiple_flutters_module" \
-    "animations" \
-    # Tracking issue: https://github.com/flutter/samples/issues/652
-    # "flutter_maps_firestore" \
-    "infinite_list" \
-    "ios_app_clip" \
-    # Tracking issue: https://github.com/flutter/samples/issues/653
-    # "isolate_example" \
-    "jsonexample" \
-    # Tracking issue: https://github.com/flutter/samples/issues/655
-    # "place_tracker" \
-    # Tracking issue: https://github.com/flutter/samples/issues/656
-    # "platform_channels" \
+    "add_to_app/fullscreen/flutter_module"
+    "add_to_app/prebuilt_module/flutter_module"
+    "add_to_app/plugin/flutter_module_using_plugin"
+    "add_to_app/books/flutter_module_books"
+    "add_to_app/multiple_flutters/multiple_flutters_module"
+    "animations"
+    "flutter_maps_firestore"
+    "infinite_list"
+    "ios_app_clip"
+    "isolate_example"
+    "jsonexample"
+    "place_tracker"
+    "platform_channels"
     "platform_design"
-    "platform_view_swift" \
-    "provider_counter" \
-    # Tracking issue: https://github.com/flutter/samples/issues/657
-    # "provider_shopper" \
-    # Tracking issue: https://github.com/flutter/samples/issues/658
-    # "testing_app" \
-    "experimental/desktop_photo_search" \
-    # https://github.com/flutter/samples/issues/659
-    # "experimental/federated_plugin/federated_plugin" \
-    "null_safety/null_safe_app" \
-    "null_safety/null_unsafe_app" \
-    "experimental/veggieseasons" \
-    "experimental/web_dashboard" \
+    "platform_view_swift"
+    "provider_counter"
+    "provider_shopper"
+    "testing_app"
+    "experimental/desktop_photo_search"
+    "experimental/federated_plugin/federated_plugin"
+    "null_safety/null_safe_app"
+    "null_safety/null_unsafe_app"
+    "experimental/veggieseasons"
+    "experimental/web_dashboard"
 )
 
 for PROJECT_NAME in "${PROJECT_NAMES[@]}"
