20feb3f90107da40d9822d488c1dc2129ebe7a25
Brett Morgan <brettmorgan@google.com>
2021-03-09 10:40:33 +1100

    Re-enable CI on beta and dev channels (#757)

--- tool/flutter_ci_script_stable.sh
+++ tool/flutter_ci_script_stable.sh
@@ -3,27 +3,31 @@
 set -e
 
 declare -ar PROJECT_NAMES=(
-    "add_to_app/android_view/flutter_module_using_plugin" \
-    "add_to_app/fullscreen/flutter_module" \
-    "add_to_app/prebuilt_module/flutter_module" \
-    "add_to_app/plugin/flutter_module_using_plugin" \
-    "add_to_app/books/flutter_module_books" \
-    "animations" \
-    "flutter_maps_firestore" \
-    "infinite_list" \
-    "ios_app_clip" \
-    "isolate_example" \
-    "jsonexample" \
-    "place_tracker" \
-    "platform_channels" \
+    "add_to_app/fullscreen/flutter_module"
+    "add_to_app/prebuilt_module/flutter_module"
+    "add_to_app/plugin/flutter_module_using_plugin"
+    "add_to_app/books/flutter_module_books"
+    # TODO(domesticmouse): tracking issue https://github.com/flutter/samples/issues/759 
+    # "add_to_app/multiple_flutters/multiple_flutters_module"
+    "animations"
+    "flutter_maps_firestore"
+    "infinite_list"
+    "ios_app_clip"
+    "isolate_example"
+    "jsonexample"
+    "place_tracker"
+    "platform_channels"
     "platform_design"
-    "platform_view_swift" \
-    "provider_counter" \
-    "provider_shopper" \
-    "testing_app" \
-    "null_safety/null_safe_app" \
-    "null_safety/null_unsafe_app" \
-    "veggieseasons" \
+    "platform_view_swift"
+    "provider_counter"
+    "provider_shopper"
+    "testing_app"
+    "veggieseasons" 
+    "experimental/desktop_photo_search"
+    "experimental/federated_plugin/federated_plugin"
+    "null_safety/null_safe_app"
+    "null_safety/null_unsafe_app"
+    "experimental/web_dashboard"
 )
 
 for PROJECT_NAME in "${PROJECT_NAMES[@]}"
