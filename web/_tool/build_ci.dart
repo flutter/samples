8598cd8b5be470ff9af968cebcbecdc64407ccb1
John Ryan <ryjohn@google.com>
2021-05-04 10:21:17 -0700

    create web/ directory in build

--- web/_tool/build_ci.dart
+++ web/_tool/build_ci.dart
@@ -11,6 +11,10 @@ main() async {
   await run('samples_index', 'pub', ['get']);
   await run('samples_index', 'pub', ['run', 'grinder', 'deploy']);
 
+  // Create the directory each Flutter Web sample lives in
+  Directory(p.join(Directory.current.path, 'samples_index', 'public', 'web'))
+      .createSync(recursive: true);
+
   for (var i = 0; i < packageDirs.length; i++) {
     var directory = packageDirs[i];
     if (directory == 'samples_index') continue;
@@ -18,12 +22,14 @@ main() async {
 
     // Create the target directory
     var directoryName = p.basename(directory);
-    var targetDirectory = p.join(
-        Directory.current.path, 'samples_index', 'public','web', directoryName);
+    var sourceBuildDir =
+        p.join(Directory.current.path, directory, 'build', 'web');
+    var targetDirectory = p.join(Directory.current.path, 'samples_index',
+        'public', 'web', directoryName);
 
     // Build the sample and copy the files
     await run(directory, 'flutter', ['pub', 'get']);
     await run(directory, 'flutter', ['build', 'web']);
-    await run(directory, 'mv', ['build/web', targetDirectory]);
+    await run(directory, 'mv', [sourceBuildDir, targetDirectory]);
   }
 }
