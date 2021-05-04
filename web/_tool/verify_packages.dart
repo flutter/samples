332c38c27ec706ea395203f93d5c1906bc6c0859
John Ryan <ryjohn@google.com>
2021-05-04 09:46:09 -0700

    Add workflow to build and deploy the sample index

--- web/_tool/verify_packages.dart
+++ web/_tool/verify_packages.dart
@@ -5,14 +5,11 @@
 import 'dart:io';
 
 import 'package:path/path.dart' as p;
+
 import 'common.dart';
 
-const _ansiGreen = 32;
-const _ansiRed = 31;
-const _ansiMagenta = 35;
-
 void main() async {
-  final packageDirs = _listPackageDirs(Directory.current)
+  final packageDirs = listPackageDirs(Directory.current)
       .map((path) => p.relative(path, from: Directory.current.path))
       .toList();
 
@@ -21,7 +18,7 @@ void main() async {
   final results = <bool>[];
   for (var i = 0; i < packageDirs.length; i++) {
     final dir = packageDirs[i];
-    logWrapped(_ansiMagenta, '\n$dir (${i + 1} of ${packageDirs.length})');
+    logWrapped(ansiMagenta, '\n$dir (${i + 1} of ${packageDirs.length})');
     results.add(await run(dir, 'flutter', [
       'pub',
       'pub',
@@ -46,19 +43,6 @@ void _printStatus(List<bool> results) {
   var success = (successCount == results.length);
   var pct = 100 * successCount / results.length;
 
-  logWrapped(success ? _ansiGreen : _ansiRed,
+  logWrapped(success ? ansiGreen : ansiRed,
       '$successCount of ${results.length} (${pct.toStringAsFixed(2)}%)');
-}
-
-Iterable<String> _listPackageDirs(Directory dir) sync* {
-  if (File('${dir.path}/pubspec.yaml').existsSync()) {
-    yield dir.path;
-  } else {
-    for (var subDir in dir
-        .listSync(followLinks: true)
-        .whereType<Directory>()
-        .where((d) => !Uri.file(d.path).pathSegments.last.startsWith('.'))) {
-      yield* _listPackageDirs(subDir);
-    }
-  }
 }
