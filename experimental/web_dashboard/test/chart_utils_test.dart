376c666f0a0bd47056e7b374cc3d69996a40b3ad
Andrew Brogdon <RedBrogdon@users.noreply.github.com>
2021-05-18 13:35:06 -0700

    Merge beta into master (#799)

--- experimental/web_dashboard/test/chart_utils_test.dart
+++ experimental/web_dashboard/test/chart_utils_test.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:test/test.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 import 'package:web_dashboard/src/api/api.dart';
 import 'package:web_dashboard/src/utils/chart_utils.dart';
