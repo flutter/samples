857457dadd1896c39d770c0afbe826fab0a83cb3
“abd99” <abdullahzakir99@gmail.com>
2021-05-19 01:30:52 +0530

    Update dependencies

---
+++ testing_app/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/abd/Developer/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/abd/FlutterSamplesRepo/samples/testing_app"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "EXCLUDED_ARCHS=arm64"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
