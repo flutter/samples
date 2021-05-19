b9abc8772067dde39fb7893d83bcdf0e94604869
Andrew Brogdon <redbrogdon@google.com>
2021-05-19 09:15:46 -0700

    Sorting additional imports

--- flutter_maps_firestore/lib/main.dart
+++ flutter_maps_firestore/lib/main.dart
@@ -3,11 +3,13 @@
 // found in the LICENSE file.
 
 import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:google_maps_webservice/places.dart';
+
 import 'api_key.dart';
 
 // Center of the Google Map
