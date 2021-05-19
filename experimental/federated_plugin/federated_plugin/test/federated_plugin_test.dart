2a6d715a7d6347fcbbc7f9bc9695313c06385458
Andrew Brogdon <redbrogdon@google.com>
2021-05-18 20:06:48 -0700

    Sorting imports in federated_plugin

--- experimental/federated_plugin/federated_plugin/test/federated_plugin_test.dart
+++ experimental/federated_plugin/federated_plugin/test/federated_plugin_test.dart
@@ -2,9 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:federated_plugin/federated_plugin.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:federated_plugin/federated_plugin.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
