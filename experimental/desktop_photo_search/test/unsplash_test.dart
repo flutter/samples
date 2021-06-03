2e292eb22f0fa3ab0567a3b85b409cc7461c388d
Brett Morgan <brett.morgan@gmail.com>
2021-06-03 17:29:50 +1000

    desktop_photo_search fix, part 2

--- experimental/desktop_photo_search/test/unsplash_test.dart
+++ experimental/desktop_photo_search/test/unsplash_test.dart
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:async';
 import 'package:desktop_photo_search/src/unsplash/photo.dart';
 import 'package:desktop_photo_search/src/unsplash/search_photos_response.dart';
 import 'package:desktop_photo_search/src/unsplash/unsplash.dart';
