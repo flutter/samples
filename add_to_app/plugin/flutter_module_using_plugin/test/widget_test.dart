70d655a1bc812880aefb81fd31995ff368e49ab2
Brett Morgan <brett.morgan@gmail.com>
2021-06-06 21:59:56 +1000

    import ordering

--- add_to_app/plugin/flutter_module_using_plugin/test/widget_test.dart
+++ add_to_app/plugin/flutter_module_using_plugin/test/widget_test.dart
@@ -6,9 +6,8 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-
 import 'package:flutter_module_using_plugin/main.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
 
 class MockCounterModel extends ChangeNotifier implements CounterModel {
