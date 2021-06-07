b7ffd16f00854efc11b0a6437041c1d1c5fa2305
filiph <filiph@users.noreply.github.com>
2021-06-07 02:48:22 +0000

    deploy: 35f43dae457fa0dac86268926af900f3ce49a293

--- web/form_app/flutter_service_worker.js
+++ web/form_app/flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "467911f197f6e63848ac8b5043ce2585",
 "main.dart.js": "04e138ac142166e85723bd6f13e52223",
-"index.html": "6842cae5c64183b62c125b79cefb6cce",
-"/": "6842cae5c64183b62c125b79cefb6cce",
+"index.html": "aef9b5c4cbfc2799e8ca89b9174fa04d",
+"/": "aef9b5c4cbfc2799e8ca89b9174fa04d",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "b14fcf3ee94e3ace300b192e9e7c8c5d",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/NOTICES": "4f7b538f85275bf3e5c623b351af4652",
