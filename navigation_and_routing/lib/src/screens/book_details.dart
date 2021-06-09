cec86663557bf5df139cc7c0835b62fc88263429
John Ryan <ryjohn@google.com>
2021-06-08 18:47:08 -0700

    add copyright headers

--- navigation_and_routing/lib/src/screens/book_details.dart
+++ navigation_and_routing/lib/src/screens/book_details.dart
@@ -1,3 +1,7 @@
+// Copyright 2021, the Flutter project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'package:bookstore/src/screens/author_details.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/link.dart';
