fc7809f142e37514a96888223561771003ec43a3
Brett Morgan <brett.morgan@gmail.com>
2021-06-09 16:12:29 +1000

    dart migrate add_to_app/android_view/flutter_module_using_plugin

--- add_to_app/android_view/flutter_module_using_plugin/lib/main.dart
+++ add_to_app/android_view/flutter_module_using_plugin/lib/main.dart
@@ -70,7 +70,7 @@ class CounterModel extends ChangeNotifier {
 /// It offers two routes, one suitable for displaying as a full screen and
 /// another designed to be part of a larger UI.
 class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -87,7 +87,7 @@ class MyApp extends StatelessWidget {
 /// Wraps [Contents] in a Material [Scaffold] so it looks correct when displayed
 /// full-screen.
 class FullScreenView extends StatelessWidget {
-  const FullScreenView({Key key}) : super(key: key);
+  const FullScreenView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -109,7 +109,7 @@ class FullScreenView extends StatelessWidget {
 class Contents extends StatelessWidget {
   final bool showExit;
 
-  const Contents({this.showExit = false, Key key}) : super(key: key);
+  const Contents({this.showExit = false, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
