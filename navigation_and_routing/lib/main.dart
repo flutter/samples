80a5e7dec589e6d19f8a33cd2b7c8f83b06bcfa7
John Ryan <ryjohn@google.com>
2021-06-30 11:12:41 -0700

    use path URL strategy on the web.

--- navigation_and_routing/lib/main.dart
+++ navigation_and_routing/lib/main.dart
@@ -3,9 +3,13 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'package:flutter/material.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 import 'src/app.dart';
 
 void main() {
+  // Use package:url_strategy until this pull request is released:
+  // https://github.com/flutter/flutter/pull/77103
+  setPathUrlStrategy();
   runApp(const Bookstore());
 }
