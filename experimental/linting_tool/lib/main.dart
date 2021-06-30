e8320b5ddb4d5d8e07fbc0804c90724569753432
“abd99” <abdullahzakir99@gmail.com>
2021-06-30 23:21:42 +0530

    Modify Names

--- experimental/linting_tool/lib/main.dart
+++ experimental/linting_tool/lib/main.dart
@@ -8,11 +8,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Linting Tool',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Linting Tool'),
     );
   }
 }
