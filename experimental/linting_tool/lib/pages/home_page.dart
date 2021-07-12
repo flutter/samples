6583955485c2582f24d6d6ebfafe963338fab01d
“abd99” <abdullahzakir99@gmail.com>
2021-07-12 23:20:17 +0530

    Implement adaptive layout

---
+++ experimental/linting_tool/lib/pages/home_page.dart
@@ -0,0 +1,14 @@
+// Copyright 2021 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Home');
+  }
+}
---
+++ experimental/linting_tool/lib/pages/saved_lints_page.dart
@@ -0,0 +1,14 @@
+// Copyright 2021 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+class SavedLintsPage extends StatelessWidget {
+  const SavedLintsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Saved Profiles');
+  }
+}
