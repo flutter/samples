58926a30f1c42363b470384e7f4e1ca81ecbfe3e
“abd99” <abdullahzakir99@gmail.com>
2021-08-10 23:54:56 +0530

    Implements default lints page

--- experimental/linting_tool/lib/pages/default_lints_page.dart
+++ experimental/linting_tool/lib/pages/default_lints_page.dart
@@ -3,13 +3,82 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
+import 'package:linting_tool/layout/adaptive.dart';
+import 'package:linting_tool/model/rules_store.dart';
+import 'package:linting_tool/pages/rules_page.dart';
+import 'package:linting_tool/theme/colors.dart';
+import 'package:provider/provider.dart';
 
 class DefaultLintsPage extends StatelessWidget {
   const DefaultLintsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // TODO(abd99): Implement DefaultLintsPage, showing a list of default lint rules.
-    return const Text('Default Profiles');
+    return Consumer<RuleStore>(
+      builder: (context, rulesStore, child) {
+        if (rulesStore.isLoading) {
+          return const CircularProgressIndicator.adaptive();
+        }
+
+        if (rulesStore.defaultProfiles.isNotEmpty) {
+          var defaultSets = rulesStore.defaultProfiles;
+          final isDesktop = isDisplayLarge(context);
+          final isTablet = isDisplayMedium(context);
+          final startPadding = isTablet
+              ? 60.0
+              : isDesktop
+                  ? 120.0
+                  : 4.0;
+          final endPadding = isTablet
+              ? 60.0
+              : isDesktop
+                  ? 120.0
+                  : 4.0;
+
+          return ListView.separated(
+            padding: EdgeInsetsDirectional.only(
+              start: startPadding,
+              end: endPadding,
+              top: isDesktop ? 28 : 0,
+              bottom: isDesktop ? kToolbarHeight : 0,
+            ),
+            cacheExtent: 5,
+            itemCount: defaultSets.length,
+            itemBuilder: (context, index) {
+              var profile = rulesStore.defaultProfiles[index];
+              return ListTile(
+                title: Text(
+                  profile.name,
+                ),
+                tileColor: AppColors.white50,
+                onTap: () {
+                  Navigator.push<void>(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => RulesPage(profile: profile),
+                    ),
+                  );
+                },
+              );
+            },
+            separatorBuilder: (context, index) => const SizedBox(height: 4),
+          );
+        }
+
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(rulesStore.error ?? 'Failed to load rules.'),
+            const SizedBox(
+              height: 16.0,
+            ),
+            IconButton(
+              onPressed: () => rulesStore.fetchRules(),
+              icon: const Icon(Icons.refresh),
+            ),
+          ],
+        );
+      },
+    );
   }
 }
