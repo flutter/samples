a511f00116ac5c5015bb98f7bf989eecefb732ad
Andrew Brogdon <redbrogdon@google.com>
2021-08-13 10:20:36 -0700

    Unbreaking flutter_books

--- tool/flutter_ci_script_dev.sh
+++ tool/flutter_ci_script_dev.sh
@@ -8,27 +8,20 @@ source "$DIR/flutter_ci_script_shared.sh"
 flutter doctor -v
 
 declare -ar PROJECT_NAMES=(
-    # TODO(redbrogdon) Replace use of mockito
-    # "add_to_app/books/flutter_module_books"
+    "add_to_app/books/flutter_module_books"
     "add_to_app/fullscreen/flutter_module"
     "add_to_app/multiple_flutters/multiple_flutters_module"
     "add_to_app/plugin/flutter_module_using_plugin"
     "add_to_app/prebuilt_module/flutter_module"
-    # TODO(redbrogdon): Update once this PR makes it to release:
-    # https://github.com/flutter/flutter/pull/79860
-    # "animations"
-    # TODO(redbrogdon): Restore during next beta branch merge.
-    # "experimental/desktop_photo_search"
+    "animations"
+    "experimental/desktop_photo_search"
     "experimental/federated_plugin/federated_plugin"
-    # TODO(redbrogdon): Restore during next beta branch merge.
-    # "experimental/web_dashboard"
+    "experimental/web_dashboard"
     "experimental/linting_tool"
     "flutter_maps_firestore"
     "infinite_list"
     "ios_app_clip"
-    # TODO(redbrogdon): Update once this PR makes it to release:
-    # https://github.com/flutter/flutter/pull/79860
-    # "isolate_example"
+    "isolate_example"
     "jsonexample"
     "null_safety/null_safe_app"
     "null_safety/null_unsafe_app"
@@ -39,8 +32,7 @@ declare -ar PROJECT_NAMES=(
     "provider_counter"
     "provider_shopper"
     "testing_app"
-    # TODO(redbrogdon): Restore during next beta branch merge.
-    # "veggieseasons"
+    "veggieseasons"
 )
 
 ci_projects "dev" "${PROJECT_NAMES[@]}"
