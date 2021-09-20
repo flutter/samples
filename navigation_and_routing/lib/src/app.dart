bf823550ed42905208100127d3de4bb7982ae184
John Ryan <ryjohn@google.com>
2021-09-20 09:05:32 -0700

    Reverts the default animation behavior to be based on the platform, (#896)
    not the kIsWeb flag:
    
    https://github.com/flutter/flutter/issues/82053

--- navigation_and_routing/lib/src/app.dart
+++ navigation_and_routing/lib/src/app.dart
@@ -64,6 +64,19 @@ class _BookstoreState extends State<Bookstore> {
           child: MaterialApp.router(
             routerDelegate: _routerDelegate,
             routeInformationParser: _routeParser,
+            // Revert back to pre-Flutter-2.5 transition behavior:
+            // https://github.com/flutter/flutter/issues/82053
+            theme: ThemeData(
+              pageTransitionsTheme: const PageTransitionsTheme(
+                builders: <TargetPlatform, PageTransitionsBuilder>{
+                  TargetPlatform.android: FadeUpwardsPageTransitionsBuilder(),
+                  TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
+                  TargetPlatform.linux: FadeUpwardsPageTransitionsBuilder(),
+                  TargetPlatform.macOS: CupertinoPageTransitionsBuilder(),
+                  TargetPlatform.windows: FadeUpwardsPageTransitionsBuilder(),
+                },
+              ),
+            ),
           ),
         ),
       );
