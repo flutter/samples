9a9f0210b722c10ade8696ab06cc117f537b7356
John Ryan <ryjohn@google.com>
2021-10-05 04:49:11 -0700

    Update mockito in form_app (#904)

--- form_app/lib/src/validation.dart
+++ form_app/lib/src/validation.dart
@@ -2,8 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart' as english_words;
+import 'package:flutter/material.dart';
 
 class FormValidationDemo extends StatefulWidget {
   const FormValidationDemo({Key key}) : super(key: key);
