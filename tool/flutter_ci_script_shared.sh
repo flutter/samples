37995bcf3ff899e468aaa6d4ef29731c1bf2d3eb
Brett Morgan <brett.morgan@gmail.com>
2021-11-20 16:11:09 +1000

    [CI]: `dart analyze --fatal-infos`

--- tool/flutter_ci_script_shared.sh
+++ tool/flutter_ci_script_shared.sh
@@ -12,7 +12,7 @@ function ci_projects () {
         flutter pub get
 
         # Run the analyzer to find any static analysis issues.
-        dart analyze
+        dart analyze --fatal-infos
 
         # Run the formatter on all the dart files to make sure everything's linted.
         dart format --output none --set-exit-if-changed .
