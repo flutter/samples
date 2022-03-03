83539ccdc88ec573c56642078bb516b6bd1cd4f4
Miguel Beltran <m@beltran.work>
2022-03-03 05:44:04 +0100

    Add setupWindow with min size to Material desktop photo search (#1037)

--- desktop_photo_search/material/lib/main.dart
+++ desktop_photo_search/material/lib/main.dart
@@ -4,10 +4,12 @@
 
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:logging/logging.dart';
 import 'package:menubar/menubar.dart' as menubar;
 import 'package:provider/provider.dart';
+import 'package:window_size/window_size.dart';
 
 import 'src/model/photo_search_model.dart';
 import 'src/unsplash/unsplash.dart';
@@ -30,6 +32,8 @@ void main() {
     exit(1);
   }
 
+  setupWindow();
+
   runApp(
     ChangeNotifierProvider<PhotoSearchModel>(
       create: (context) => PhotoSearchModel(
@@ -38,6 +42,16 @@ void main() {
       child: const UnsplashSearchApp(),
     ),
   );
+}
+
+const double kWindowWidth = 1024;
+const double kWindowHeight = 800;
+
+void setupWindow() {
+  if (!kIsWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS)) {
+    WidgetsFlutterBinding.ensureInitialized();
+    setWindowMinSize(const Size(kWindowWidth, kWindowHeight));
+  }
 }
 
 class UnsplashSearchApp extends StatelessWidget {
