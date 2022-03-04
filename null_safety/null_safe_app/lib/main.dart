bbc51bcbeb3ab38ba0f39a533a7ab3b63c41a4cd
Miquel Beltran <miquelbeltran@users.noreply.github.com>
2022-03-04 12:11:36 +0100

    Add window setup to null_safe_app

--- null_safety/null_safe_app/lib/main.dart
+++ null_safety/null_safe_app/lib/main.dart
@@ -2,10 +2,26 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:window_size/window_size.dart';
 
 import 'goodapp.dart';
 
 void main() {
+  setupWindow();
   runApp(const GoodMyApp());
+}
+
+const double kWindowWidth = 400;
+const double kWindowHeight = 400;
+
+void setupWindow() {
+  if (!kIsWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS)) {
+    WidgetsFlutterBinding.ensureInitialized();
+    setWindowTitle('Weather');
+    setWindowMinSize(const Size(kWindowWidth, kWindowHeight));
+  }
 }
