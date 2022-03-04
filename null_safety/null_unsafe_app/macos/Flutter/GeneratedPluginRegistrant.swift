09af360a11ac8d78db63b2501438e09f249be25e
Miquel Beltran <miquelbeltran@users.noreply.github.com>
2022-03-04 12:45:13 +0100

    Add macos and windows folders

---
+++ null_safety/null_unsafe_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import window_size
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  WindowSizePlugin.register(with: registry.registrar(forPlugin: "WindowSizePlugin"))
+}
