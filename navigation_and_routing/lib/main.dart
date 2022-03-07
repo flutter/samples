d075347baea766734f7d94a7d823571f95a89d02
Miguel Beltran <m@beltran.work>
2022-03-07 14:39:30 +0100

    rename constants

--- navigation_and_routing/lib/main.dart
+++ navigation_and_routing/lib/main.dart
@@ -27,20 +27,20 @@ void main() {
   runApp(const Bookstore());
 }
 
-const double kWindowWidth = 480;
-const double kWindowHeight = 854;
+const double windowWidth = 480;
+const double windowHeight = 854;
 
 void setupWindow() {
   if (!kIsWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS)) {
     WidgetsFlutterBinding.ensureInitialized();
     setWindowTitle('Navigation and routing');
-    setWindowMinSize(const Size(kWindowWidth, kWindowHeight));
-    setWindowMaxSize(const Size(kWindowWidth, kWindowHeight));
+    setWindowMinSize(const Size(windowWidth, windowHeight));
+    setWindowMaxSize(const Size(windowWidth, windowHeight));
     getCurrentScreen().then((screen) {
       setWindowFrame(Rect.fromCenter(
         center: screen!.frame.center,
-        width: kWindowWidth,
-        height: kWindowHeight,
+        width: windowWidth,
+        height: windowHeight,
       ));
     });
   }
