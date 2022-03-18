a481c264cde28c1c511e2cc43d33c7bdef47f110
Brett Morgan <brett.morgan@gmail.com>
2022-03-19 08:37:04 +1000

    Remove git:// url, update deps, fix `destop_photo_search` (#1052)

--- jsonexample/windows/flutter/generated_plugin_registrant.cc
+++ jsonexample/windows/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,9 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <window_size/window_size_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  WindowSizePluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("WindowSizePlugin"));
 }
