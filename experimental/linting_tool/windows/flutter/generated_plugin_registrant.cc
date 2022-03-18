cd5363e568faddd7315a116b2b5792b631074ce7
Brett Morgan <brett.morgan@gmail.com>
2022-03-18 04:41:46 +0000

    Flutter 2.12 beta dependency bump

--- experimental/linting_tool/windows/flutter/generated_plugin_registrant.cc
+++ experimental/linting_tool/windows/flutter/generated_plugin_registrant.cc
@@ -6,13 +6,13 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <file_selector_windows/file_selector_plugin.h>
+#include <file_selector_windows/file_selector_windows.h>
 #include <url_launcher_windows/url_launcher_windows.h>
 #include <window_size/window_size_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  FileSelectorPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("FileSelectorPlugin"));
+  FileSelectorWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FileSelectorWindows"));
   UrlLauncherWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherWindows"));
   WindowSizePluginRegisterWithRegistrar(
