148b18ba59c346a27b03db4e728056720c2ee708
Brett Morgan <brett.morgan@gmail.com>
2022-03-24 15:06:58 +1000

    `desktop_photo_search`: update depenndencies

--- desktop_photo_search/material/macos/Flutter/GeneratedPluginRegistrant.swift
+++ desktop_photo_search/material/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,9 +8,11 @@ import Foundation
 import file_selector_macos
 import menubar
 import url_launcher_macos
+import window_size
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   MenubarPlugin.register(with: registry.registrar(forPlugin: "MenubarPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  WindowSizePlugin.register(with: registry.registrar(forPlugin: "WindowSizePlugin"))
 }
