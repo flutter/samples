f86b5f3eab3dc106398bfee8ffa3e79aa24f0109
domesticmouse <domesticmouse@users.noreply.github.com>
2022-04-18 21:34:18 +0000

    deploy: 091cf93853ce07b392ee9abc8afbf224c6469596

--- web/slide_puzzle/flutter_service_worker.js
+++ web/slide_puzzle/flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "main.dart.js": "7f64a17e6764e8896b44e1edcf323cc6",
-"index.html": "baa0f009553ae7310f8ad3b469a7117f",
-"/": "baa0f009553ae7310f8ad3b469a7117f",
+"index.html": "0bf388ffbb7e25d57cac0259aed9dac9",
+"/": "0bf388ffbb7e25d57cac0259aed9dac9",
 "favicon.png": "7a26ea9d4e61122fc023b84e5acdb148",
 "assets/fonts/MaterialIcons-Regular.otf": "7e7a6cccddf6d7b20012a548461d5d81",
 "assets/AssetManifest.json": "1b1637a32286fee38b7df3f3ce2da987",
