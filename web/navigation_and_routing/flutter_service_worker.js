50e3ce7548525ea014bf8c5379e86ce4aac4fc58
domesticmouse <domesticmouse@users.noreply.github.com>
2022-04-18 21:20:22 +0000

    deploy: 1371134dd78b1abe59f2493550b2aee646c30c8d

--- web/navigation_and_routing/flutter_service_worker.js
+++ web/navigation_and_routing/flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "main.dart.js": "32f407fce4d24c3abdd7466f38c12d4f",
-"index.html": "7a0e8328ea3ac7a2cdfcb8b910d1ae2e",
-"/": "7a0e8328ea3ac7a2cdfcb8b910d1ae2e",
+"index.html": "fb0357ffbc6a97bfad7a5177cb2f6ce5",
+"/": "fb0357ffbc6a97bfad7a5177cb2f6ce5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "assets/fonts/MaterialIcons-Regular.otf": "7e7a6cccddf6d7b20012a548461d5d81",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
