c44cf3e5aea9c427fde9cdd78690ce0270a4b011
domesticmouse <domesticmouse@users.noreply.github.com>
2022-04-18 20:58:59 +0000

    deploy: 8bbfb09c9bd2a94adbb97f7f854eaf7bf5ad3faf

--- web/form_app/flutter_service_worker.js
+++ web/form_app/flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "main.dart.js": "4b356c1db392364dabd460bc34b333f2",
-"index.html": "beb7d324a550db5d15cba8f0b4db33e7",
-"/": "beb7d324a550db5d15cba8f0b4db33e7",
+"index.html": "d74056bde6409dae99e53d7a428f0b41",
+"/": "d74056bde6409dae99e53d7a428f0b41",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "assets/fonts/MaterialIcons-Regular.otf": "7e7a6cccddf6d7b20012a548461d5d81",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
--- web/slide_puzzle/flutter_service_worker.js
+++ web/slide_puzzle/flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "main.dart.js": "7f64a17e6764e8896b44e1edcf323cc6",
-"index.html": "58af17079149db1c647d7cc01f6e0cf9",
-"/": "58af17079149db1c647d7cc01f6e0cf9",
+"index.html": "1fa5f347008d8fe19660ab924baaefed",
+"/": "1fa5f347008d8fe19660ab924baaefed",
 "favicon.png": "7a26ea9d4e61122fc023b84e5acdb148",
 "assets/fonts/MaterialIcons-Regular.otf": "7e7a6cccddf6d7b20012a548461d5d81",
 "assets/AssetManifest.json": "1b1637a32286fee38b7df3f3ce2da987",
