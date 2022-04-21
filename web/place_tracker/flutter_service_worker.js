186215a6b0e2f39585208d27d813a533b1658a4c
domesticmouse <domesticmouse@users.noreply.github.com>
2022-04-21 04:02:40 +0000

    deploy: 720b2653ad4b1f0f20e6080896e60b89c882948d

--- web/place_tracker/flutter_service_worker.js
+++ web/place_tracker/flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "main.dart.js": "63a5beb758fa3d9b4076efc0f1705d3a",
-"index.html": "8e1ef5ff222d87c70f97104b2198f3cd",
-"/": "8e1ef5ff222d87c70f97104b2198f3cd",
+"index.html": "6fd7fc767b20a222ffaee10e8b5a5bb5",
+"/": "6fd7fc767b20a222ffaee10e8b5a5bb5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "assets/fonts/MaterialIcons-Regular.otf": "7e7a6cccddf6d7b20012a548461d5d81",
 "assets/AssetManifest.json": "aa929e7225d630c8eda7dc6358d13844",
