20e0209d379fa99e17d47cafdb756792222c9ca0
Filip Hracek <filiph@users.noreply.github.com>
2022-05-02 19:05:14 +0200

    Update game_template/lib/src/audio/songs.dart
    Co-authored-by: Parker Lougheed <parlough@gmail.com>

--- game_template/lib/src/audio/songs.dart
+++ game_template/lib/src/audio/songs.dart
@@ -1,19 +1,19 @@
 const Set<Song> songs = {
   // Filenames with whitespace break package:audioplayers on iOS
   // (as of February 2022), so we use no whitespace.
-  Song('Mr_Smith-Azul.mp3', 'Azul'),
-  Song('Mr_Smith-Sonorus.mp3', 'Sonorus'),
-  Song('Mr_Smith-Sunday_Solitude.mp3', 'SundaySolitude'),
+  Song('Mr_Smith-Azul.mp3', 'Azul', artist: 'Mr Smith'),
+  Song('Mr_Smith-Sonorus.mp3', 'Sonorus', artist: 'Mr Smith'),
+  Song('Mr_Smith-Sunday_Solitude.mp3', 'SundaySolitude', artist: 'Mr Smith'),
 };
 
 class Song {
   final String filename;
 
-  final String author = 'Mr Smith';
-
   final String name;
 
-  const Song(this.filename, this.name);
+  final String? artist;
+
+  const Song(this.filename, this.name, {this.artist});
 
   @override
   String toString() => 'Song<$filename>';
