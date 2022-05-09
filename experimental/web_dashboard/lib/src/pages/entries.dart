8fc5b03c77df581790bbc6fdaddec8a24c298672
Craig Labenz <craig.labenz@gmail.com>
2022-05-09 16:10:46 -0700

    updated Dart SDK in pubspec

--- experimental/web_dashboard/lib/src/pages/entries.dart
+++ experimental/web_dashboard/lib/src/pages/entries.dart
@@ -2,7 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart' as intl;
