a978cf037185a8c725ecf5622ea7c3ce1d41e680
Craig Labenz <craig.labenz@gmail.com>
2022-05-09 16:22:58 -0700

    fixed private class issue

--- platform_view_swift/lib/main.dart
+++ platform_view_swift/lib/main.dart
@@ -30,10 +30,10 @@ class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  HomePageState createState() => HomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> {
   static const MethodChannel _methodChannel =
       MethodChannel('dev.flutter.sample/platform_view_swift');
 
