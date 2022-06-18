e4d4d0f48d06902082a2897ee8599430f4f14052
Rich Hall <rjhall@gmail.com>
2022-06-18 10:53:07 +1000

    Changes to package import statements to prevent errors when using FirebaseCrashlytics code (#1309)

--- game_template/lib/main.dart
+++ game_template/lib/main.dart
@@ -2,6 +2,11 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+// Uncomment the following lines when enabling Firebase Crashlytics
+// import 'dart:io';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'firebase_options.dart';
+
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -34,7 +39,8 @@ import 'src/style/snack_bar.dart';
 import 'src/win_game/win_game_screen.dart';
 
 Future<void> main() async {
-  // Uncomment the following lines to enable Firebase Crashlytics.
+  // To enable Firebase Crashlytics, uncomment the following lines and
+  // the import statements at the top of this file.
   // See the 'Crashlytics' section of the main README.md file for details.
 
   FirebaseCrashlytics? crashlytics;
