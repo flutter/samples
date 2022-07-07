652d67633a4b55ff5c1d655cf51766531d03d425
Qun Cheng <quncheng@google.com>
2022-07-07 11:27:13 -0700

    Added m3 icon buttons

--- material_3_demo/lib/component_screen.dart
+++ material_3_demo/lib/component_screen.dart
@@ -25,6 +25,9 @@ class ComponentScreen extends StatelessWidget {
                 _colDivider,
                 const Buttons(),
                 _colDivider,
+                _colDivider,
+                const IconToggleButtons(),
+                _colDivider,
                 const FloatingActionButtons(),
                 _colDivider,
                 const Cards(),
@@ -514,4 +517,199 @@ class _NavigationRailSectionState extends State<NavigationRailSection> {
       },
     );
   }
+}
+
+class IconToggleButtons extends StatefulWidget {
+  const IconToggleButtons({super.key});
+
+  @override
+  State<IconToggleButtons> createState() => _IconToggleButtonsState();
+}
+
+class _IconToggleButtonsState extends State<IconToggleButtons> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Column(mainAxisAlignment: MainAxisAlignment.center,
+                // Standard IconButton
+                children: const <Widget>[
+                  IconToggleButton(isEnabled: true),
+                  _colDivider,
+                  IconToggleButton(isEnabled: false),
+                ]),
+            Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const <Widget>[
+                  // Filled IconButton
+                  IconToggleButton(
+                    isEnabled: true,
+                    getDefaultStyle: enabledFilledButtonStyle,
+                  ),
+                  _colDivider,
+                  IconToggleButton(
+                    isEnabled: false,
+                    getDefaultStyle: disabledFilledButtonStyle,
+                  )
+                ]),
+            Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const <Widget>[
+                  // Filled Tonal IconButton
+                  IconToggleButton(
+                    isEnabled: true,
+                    getDefaultStyle: enabledFilledTonalButtonStyle,
+                  ),
+                  _colDivider,
+                  IconToggleButton(
+                    isEnabled: false,
+                    getDefaultStyle: disabledFilledTonalButtonStyle,
+                  ),
+                ]),
+            Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const <Widget>[
+                  // Outlined IconButton
+                  IconToggleButton(
+                    isEnabled: true,
+                    getDefaultStyle: enabledOutlinedButtonStyle,
+                  ),
+                  _colDivider,
+                  IconToggleButton(
+                    isEnabled: false,
+                    getDefaultStyle: disabledOutlinedButtonStyle,
+                  ),
+                ]),
+          ]),
+    );
+  }
+}
+
+class IconToggleButton extends StatefulWidget {
+  const IconToggleButton(
+      {required this.isEnabled, this.getDefaultStyle, super.key});
+
+  final bool isEnabled;
+  final ButtonStyle? Function(bool, ColorScheme)? getDefaultStyle;
+
+  @override
+  State<IconToggleButton> createState() => _IconToggleButtonState();
+}
+
+class _IconToggleButtonState extends State<IconToggleButton> {
+  bool selected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final ColorScheme colors = Theme.of(context).colorScheme;
+    final VoidCallback? onPressed = widget.isEnabled
+        ? () {
+            setState(() {
+              selected = !selected;
+            });
+          }
+        : null;
+    ButtonStyle? style;
+    if (widget.getDefaultStyle != null) {
+      style = widget.getDefaultStyle!(selected, colors);
+    }
+
+    return IconButton(
+      visualDensity: VisualDensity.standard,
+      isSelected: selected,
+      icon: const Icon(Icons.settings_outlined),
+      selectedIcon: const Icon(Icons.settings),
+      onPressed: onPressed,
+      style: style,
+    );
+  }
+}
+
+ButtonStyle enabledFilledButtonStyle(bool selected, ColorScheme colors) {
+  return IconButton.styleFrom(
+    foregroundColor: selected ? colors.onPrimary : colors.primary,
+    backgroundColor: selected ? colors.primary : colors.surfaceVariant,
+    disabledForegroundColor: colors.onSurface.withOpacity(0.38),
+    disabledBackgroundColor: colors.onSurface.withOpacity(0.12),
+    hoverColor: selected
+        ? colors.onPrimary.withOpacity(0.08)
+        : colors.primary.withOpacity(0.08),
+    focusColor: selected
+        ? colors.onPrimary.withOpacity(0.12)
+        : colors.primary.withOpacity(0.12),
+    highlightColor: selected
+        ? colors.onPrimary.withOpacity(0.12)
+        : colors.primary.withOpacity(0.12),
+  );
+}
+
+ButtonStyle disabledFilledButtonStyle(bool selected, ColorScheme colors) {
+  return IconButton.styleFrom(
+    disabledForegroundColor: colors.onSurface.withOpacity(0.38),
+    disabledBackgroundColor: colors.onSurface.withOpacity(0.12),
+  );
+}
+
+ButtonStyle enabledFilledTonalButtonStyle(bool selected, ColorScheme colors) {
+  return IconButton.styleFrom(
+    foregroundColor:
+        selected ? colors.onSecondaryContainer : colors.onSurfaceVariant,
+    backgroundColor:
+        selected ? colors.secondaryContainer : colors.surfaceVariant,
+    hoverColor: selected
+        ? colors.onSecondaryContainer.withOpacity(0.08)
+        : colors.onSurfaceVariant.withOpacity(0.08),
+    focusColor: selected
+        ? colors.onSecondaryContainer.withOpacity(0.12)
+        : colors.onSurfaceVariant.withOpacity(0.12),
+    highlightColor: selected
+        ? colors.onSecondaryContainer.withOpacity(0.12)
+        : colors.onSurfaceVariant.withOpacity(0.12),
+  );
+}
+
+ButtonStyle disabledFilledTonalButtonStyle(bool selected, ColorScheme colors) {
+  return IconButton.styleFrom(
+    disabledForegroundColor: colors.onSurface.withOpacity(0.38),
+    disabledBackgroundColor: colors.onSurface.withOpacity(0.12),
+  );
+}
+
+ButtonStyle enabledOutlinedButtonStyle(bool selected, ColorScheme colors) {
+  return IconButton.styleFrom(
+    backgroundColor: selected ? colors.inverseSurface : null,
+    hoverColor: selected
+        ? colors.onInverseSurface.withOpacity(0.08)
+        : colors.onSurfaceVariant.withOpacity(0.08),
+    focusColor: selected
+        ? colors.onInverseSurface.withOpacity(0.12)
+        : colors.onSurfaceVariant.withOpacity(0.12),
+    highlightColor: selected
+        ? colors.onInverseSurface.withOpacity(0.12)
+        : colors.onSurface.withOpacity(0.12),
+    side: BorderSide(color: colors.outline),
+  ).copyWith(
+    foregroundColor:
+        MaterialStateProperty.resolveWith((Set<MaterialState> states) {
+      if (states.contains(MaterialState.selected)) {
+        return colors.onInverseSurface;
+      }
+      if (states.contains(MaterialState.pressed)) {
+        return colors.onSurface;
+      }
+      return null;
+    }),
+  );
+}
+
+ButtonStyle disabledOutlinedButtonStyle(bool selected, ColorScheme colors) {
+  return IconButton.styleFrom(
+    disabledForegroundColor: colors.onSurface.withOpacity(0.38),
+    disabledBackgroundColor:
+        selected ? colors.onSurface.withOpacity(0.12) : null,
+    side: selected ? null : BorderSide(color: colors.outline.withOpacity(0.12)),
+  );
 }
