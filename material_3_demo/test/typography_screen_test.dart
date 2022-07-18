00217730cf28b76c3d6c0e1a66f72bda125d64a9
Qun Cheng <quncheng@google.com>
2022-07-17 19:59:05 -0700

    Modified the analysis options config to drop this lint.

--- material_3_demo/test/typography_screen_test.dart
+++ material_3_demo/test/typography_screen_test.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// ignore_for_file: avoid_types_on_closure_parameters
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:material_3_demo/main.dart';
