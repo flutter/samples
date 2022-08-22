3688b75c0c577d503677e76823fe793fba7bf65d
Aaron Clarke <aaclarke@google.com>
2022-08-22 14:56:20 -0700

    [add-to-app/books] Added missing class declarations from the pigeon file.

--- add_to_app/books/flutter_module_books/pigeon/schema.dart
+++ add_to_app/books/flutter_module_books/pigeon/schema.dart
@@ -2,8 +2,21 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter_module_books/api.dart';
 import 'package:pigeon/pigeon.dart';
+
+class Book {
+  String? title;
+  String? subtitle;
+  String? author;
+  String? summary;
+  String? publishDate;
+  int? pageCount;
+  Thumbnail? thumbnail;
+}
+
+class Thumbnail {
+  String? url;
+}
 
 @FlutterApi()
 abstract class FlutterBookApi {
