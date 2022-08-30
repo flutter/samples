c006254fecd2f5cf031f499e1df9524210273d9e
Brett Morgan <brett.morgan@gmail.com>
2022-08-30 12:23:29 +1000

    Fix `master` CI (#1396)
    * Elide `add_to_app/fullscreen/flutter_module`
    
    * Elide `add_to_app/multiple_flutters/multiple_flutters_module`
    
    * Elide `add_to_app/plugin/flutter_module_using_plugin`
    
    * Elide `add_to_app/prebuilt_module/flutter_module`
    
    * Elide `android_splash_screen`
    
    * Elide `animations`
    
    * Elide `experimental/federated_plugin/federated_plugin`
    
    * Elide `game_template`
    
    * Elide `infinite_list`
    
    * Elide `jsonexample`
    
    * Elide `navigation_and_routing`
    
    * Elide `platform_design`
    
    * Elide `platform_view_swift`
    
    * Elide `provider_counter`

--- tool/flutter_ci_script_master.sh
+++ tool/flutter_ci_script_master.sh
@@ -10,17 +10,24 @@ flutter doctor -v
 declare -ar PROJECT_NAMES=(
     # TODO(domesticmouse): re-enable when stable increments
     # "add_to_app/books/flutter_module_books"
-    "add_to_app/fullscreen/flutter_module"
-    "add_to_app/multiple_flutters/multiple_flutters_module"
-    "add_to_app/plugin/flutter_module_using_plugin"
-    "add_to_app/prebuilt_module/flutter_module"
-    "android_splash_screen"
-    "animations"
+    # TODO(domesticmouse): headline5 deprecated
+    # "add_to_app/fullscreen/flutter_module"
+    # TODO(domesticmouse): headline4 is deprecated
+    # "add_to_app/multiple_flutters/multiple_flutters_module"
+    # TODO(domesticmouse): headline5 deprecated
+    # "add_to_app/plugin/flutter_module_using_plugin"
+    # TODO(domesticmouse): headline5 deprecated
+    # "add_to_app/prebuilt_module/flutter_module"
+    # TODO(domesticmouse): headline4 is deprecated
+    # "android_splash_screen"
+    # TODO(domesticmouse): headline4 is deprecated
+    # "animations"
     # TODO(domesticmouse): re-enable when stable increments
     # "desktop_photo_search/fluent_ui"
     # TODO(domesticmouse): re-enable when stable increments
     # "desktop_photo_search/material"
-    "experimental/federated_plugin/federated_plugin"
+    # TODO(domesticmouse): headline5 deprecated
+    # "experimental/federated_plugin/federated_plugin"
     # TODO(domesticmouse): re-enable when stable increments
     # "experimental/web_dashboard"
     # Needs a reformat to work on master
@@ -29,24 +36,31 @@ declare -ar PROJECT_NAMES=(
     "flutter_maps_firestore"
     # TODO(domesticmouse): re-enable when stable increments
     # "form_app"
-    "game_template"
-    "infinite_list"
+    # TODO(domesticmouse): bodyText2 is deprecated
+    # "game_template"
+    # TODO(domesticmouse): headline6 is deprecated
+    # "infinite_list"
     "ios_app_clip"
     # TODO(domesticmouse): re-enable when stable increments
     # "isolate_example"
-    "jsonexample"
+    # TODO(domesticmouse): bodyText2 is deprecated
+    # "jsonexample"
     # TODO(domesticmouse): re-enable when stable increments
     # "material_3_demo"
-    "navigation_and_routing"
+    # TODO(domesticmouse): headline4 is deprecated
+    # "navigation_and_routing"
     "null_safety/null_safe_app"
     "null_safety/null_unsafe_app"
     # TODO(domesticmouse): re-enable when stable increments
     # "place_tracker"
     # TODO(domesticmouse): re-enable when stable increments
     # "platform_channels"
-    "platform_design"
-    "platform_view_swift"
-    "provider_counter"
+    # TODO(domesticmouse): bodyText2 is deprecated
+    # "platform_design"
+    # TODO(domesticmouse): subtitle2 is deprecated
+    # "platform_view_swift"
+    # TODO(domesticmouse): headline4 is deprecated
+    # "provider_counter"
     # TODO(domesticmouse): re-enable when stable increments
     # "provider_shopper"
     # TODO(domesticmouse): re-enable when stable increments
