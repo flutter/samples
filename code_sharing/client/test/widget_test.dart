fbec884f626a552ca1510a908768849311878766
Craig Labenz <craig.labenz@gmail.com>
2022-10-04 13:23:23 -0700

    CI changes to code_sharing

--- code_sharing/client/test/widget_test.dart
+++ code_sharing/client/test/widget_test.dart
@@ -1,1 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:client/main.dart';
 
+void main() {
+  group('App should', () {
+    testWidgets('build its widgets', (WidgetTester tester) async {
+      await tester.pumpWidget(const MyApp());
+    });
+  });
+}
