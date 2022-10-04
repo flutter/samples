9020da00f590fcd0164d2a8f08f6fad1fd5a4389
Craig Labenz <craig.labenz@gmail.com>
2022-10-04 14:20:52 -0700

    fixed test with DI

--- code_sharing/client/lib/main.dart
+++ code_sharing/client/lib/main.dart
@@ -10,11 +10,23 @@ final scheme = Platform.environment['SERVER_SCHEME'] ?? 'http';
 final serverUrl = Uri.parse('$scheme://$host:$port/');
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const MyApp(
+      getCount: getCount,
+      increment: increment,
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({
+    super.key,
+    required this.getCount,
+    required this.increment,
+  });
+
+  final Future<int> Function() getCount;
+  final Future<int> Function(int) increment;
 
   @override
   Widget build(BuildContext context) {
@@ -23,15 +35,26 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(
+        title: 'Flutter Demo Home Page',
+        getCount: getCount,
+        increment: increment,
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({
+    super.key,
+    required this.title,
+    required this.getCount,
+    required this.increment,
+  });
 
   final String title;
+  final Future<int> Function() getCount;
+  final Future<int> Function(int) increment;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -45,13 +68,13 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
-    getCount().then((int val) => _counter = val);
+    widget.getCount().then((int val) => _counter = val);
   }
 
   void _incrementCounter() {
     localClicks += 1;
     setState(() => isWriting = true);
-    increment(localClicks).then(
+    widget.increment(localClicks).then(
       (int val) => setState(
         () {
           _counter = val;
--- code_sharing/client/test/widget_test.dart
+++ code_sharing/client/test/widget_test.dart
@@ -4,7 +4,12 @@ import 'package:client/main.dart';
 void main() {
   group('App should', () {
     testWidgets('build its widgets', (WidgetTester tester) async {
-      await tester.pumpWidget(const MyApp());
+      await tester.pumpWidget(
+        MyApp(
+          getCount: () => Future.value(1),
+          increment: (int x) => Future.value(x),
+        ),
+      );
     });
   });
 }
