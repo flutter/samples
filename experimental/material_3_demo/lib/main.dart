efd9d1affad665dce0769454325a084b2c9d2dca
Qun Cheng <quncheng@google.com>
2022-12-07 14:53:39 -0800

    Added animation for the transition between navBar and navRail

--- experimental/material_3_demo/lib/main.dart
+++ experimental/material_3_demo/lib/main.dart
@@ -23,6 +23,8 @@ class Material3Demo extends StatefulWidget {
 // screenWidthThreshold; otherwise, NavigationBar is used for navigation.
 const double narrowScreenWidthThreshold = 450;
 
+const double transitionLength = 250;
+
 const Color m3BaseColor = Color(0xff6750a4);
 const List<Color> colorOptions = [
   m3BaseColor,
@@ -176,38 +178,126 @@ class _Material3DemoState extends State<Material3Demo> {
       title: 'Material 3',
       themeMode: useLightMode ? ThemeMode.light : ThemeMode.dark,
       theme: themeData,
-      home: LayoutBuilder(builder: (context, constraints) {
-        if (constraints.maxWidth < narrowScreenWidthThreshold) {
+      home: NavigationTransition(
+        appBar: createAppBar(),
+        body: createScreenFor(ScreenSelected.values[screenIndex], false),
+        navigationRail: NavigationRailSection(
+          onSelectItem: handleScreenChanged,
+          selectedIndex: screenIndex),
+        navigationBar: NavigationBars(
+          onSelectItem: handleScreenChanged,
+          selectedIndex: screenIndex,
+          isExampleBar: false,
+        ),
+      ),
+    );
+  }
+}
+
+class NavigationTransition extends StatefulWidget {
+  const NavigationTransition({
+    super.key,
+    required this.navigationRail,
+    required this.navigationBar,
+    required this.appBar,
+    required this.body
+  });
+
+  final Widget navigationRail;
+  final Widget navigationBar;
+  final PreferredSizeWidget appBar;
+  final Widget body;
+
+  @override
+  State<NavigationTransition> createState() => _NavigationTransitionState();
+}
+
+class _NavigationTransitionState extends State<NavigationTransition> with SingleTickerProviderStateMixin {
+  late final AnimationController controller;
+  late final CurvedAnimation railAnimation;
+  late final ReverseAnimation barAnimation;
+  bool controllerInitialized = false;
+  bool showDivider = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      duration: Duration(milliseconds: transitionLength.toInt() * 2),
+      value: 0,
+      vsync: this,
+    );
+
+    barAnimation = ReverseAnimation(
+      CurvedAnimation(
+        parent: controller,
+        curve: const Interval(0.0, 0.5),
+      ),
+    );
+
+    railAnimation = CurvedAnimation(
+      parent: controller,
+      curve: const Interval(0.5, 1.0),
+    );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    final double width = MediaQuery.of(context).size.width;
+    final AnimationStatus status = controller.status;
+    if (width > 600) {
+      showDivider = true;
+      if (status != AnimationStatus.forward && status != AnimationStatus.completed) {
+        controller.forward();
+      }
+    } else {
+      showDivider = false;
+      if (status != AnimationStatus.reverse && status != AnimationStatus.dismissed) {
+        controller.reverse();
+      }
+    }
+    if (!controllerInitialized) {
+      controllerInitialized = true;
+      controller.value = width > 600 ? 1 : 0;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+
+    return AnimatedBuilder(
+        animation: controller,
+        builder: (context, child) {
           return Scaffold(
-            appBar: createAppBar(),
-            body: Row(children: <Widget>[
-              createScreenFor(ScreenSelected.values[screenIndex], false),
-            ]),
-            bottomNavigationBar: NavigationBars(
-              onSelectItem: handleScreenChanged,
-              selectedIndex: screenIndex,
-              isExampleBar: false,
+            appBar: widget.appBar,
+            body: Row(
+              children: <Widget>[
+                RailTransition(
+                  animation: railAnimation,
+                  backgroundColor: colorScheme.surface,
+                  child: widget.navigationRail,
+                ),
+                railAnimation.isDismissed ? const SizedBox() : const VerticalDivider(width: 1),
+                widget.body,
+              ],
             ),
-          );
-        } else {
-          return Scaffold(
-            appBar: createAppBar(),
-            body: SafeArea(
-              bottom: false,
-              top: false,
-              child: Row(
-                children: <Widget>[
-                  NavigationRailSection(
-                      onSelectItem: handleScreenChanged,
-                      selectedIndex: screenIndex),
-                  const VerticalDivider(width: 1,),
-                  createScreenFor(ScreenSelected.values[screenIndex], true),
-                ],
-              ),
+            bottomNavigationBar: BarTransition(
+              animation: barAnimation,
+              backgroundColor: colorScheme.surface,
+              child: widget.navigationBar,
             ),
           );
         }
-      }),
     );
   }
 }
@@ -260,3 +350,135 @@ final List<NavigationRailDestination> navRailDestinations = appBarDestinations
       ),
       label: Text(destination.label),),)
     .toList();
+
+class SizeAnimation extends CurvedAnimation {
+  SizeAnimation(Animation<double> parent) : super(
+    parent: parent,
+    curve: const Interval(
+      50 / transitionLength, 200 / transitionLength,
+      curve: Curves.easeInOutCubicEmphasized,
+    ),
+    reverseCurve: Interval(
+      0, 50 / transitionLength,
+      curve: Curves.easeInOutCubicEmphasized.flipped,
+    ),
+  );
+}
+
+class OffsetAnimation extends CurvedAnimation {
+  OffsetAnimation(Animation<double> parent) : super(
+    parent: parent,
+    curve: const Interval(
+      100 / transitionLength, 1.0,
+      curve: Curves.easeInOutCubicEmphasized,
+    ),
+    reverseCurve: Interval(
+      0, 50 / transitionLength,
+      curve: Curves.easeInOutCubicEmphasized.flipped,
+    ),
+  );
+}
+
+class RailTransition extends StatefulWidget {
+  const RailTransition({ super.key, required this.animation, required this.backgroundColor, required this.child });
+
+  final Animation<double> animation;
+  final Widget child;
+  final Color backgroundColor;
+
+  @override
+  State<RailTransition> createState() => _RailTransition();
+
+}
+
+class _RailTransition extends State<RailTransition> {
+  late Animation<Offset> offsetAnimation;
+  late Animation<double> widthAnimation;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    // The animations are only rebuilt by this method when the text
+    // direction changes because this widget only depends on Directionality.
+    final bool ltr =  Directionality.of(context) == TextDirection.ltr;
+
+    widthAnimation = Tween<double>(
+      begin: 0,
+      end: 1,
+    ).animate(SizeAnimation(widget.animation));
+
+    offsetAnimation = Tween<Offset>(
+      begin: ltr ? const Offset(-1, 0) : const Offset(1, 0),
+      end: Offset.zero,
+    ).animate(OffsetAnimation(widget.animation));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRect(
+      child: DecoratedBox(
+        decoration: BoxDecoration(color: widget.backgroundColor),
+        child: Align(
+          alignment: Alignment.topLeft,
+          widthFactor: widthAnimation.value,
+          child: FractionalTranslation(
+            translation: offsetAnimation.value,
+            child: widget.child,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+class BarTransition extends StatefulWidget {
+  const BarTransition({ super.key, required this.animation, required this.backgroundColor, required this.child });
+
+  final Animation<double> animation;
+  final Color backgroundColor;
+  final Widget child;
+
+  @override
+  State<BarTransition> createState() => _BarTransition();
+
+}
+
+class _BarTransition extends State<BarTransition> {
+  late final Animation<Offset> offsetAnimation;
+  late final Animation<double> heightAnimation;
+
+
+  @override
+  void initState() {
+    super.initState();
+
+    offsetAnimation = Tween<Offset>(
+      begin: const Offset(0, 1),
+      end: Offset.zero,
+    ).animate(OffsetAnimation(widget.animation));
+
+    heightAnimation = Tween<double>(
+      begin: 0,
+      end: 1,
+    ).animate(SizeAnimation(widget.animation));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRect(
+      child: DecoratedBox(
+        decoration: BoxDecoration(color: widget.backgroundColor),
+        child: Align(
+          alignment: Alignment.topLeft,
+          heightFactor: heightAnimation.value,
+          child: FractionalTranslation(
+            translation: offsetAnimation.value,
+            child: widget.child,
+          ),
+        ),
+      ),
+    );
+  }
+}
