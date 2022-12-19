0d4ca9179835c5612d68d540e0fa652e0a037c05
Miguel Beltran <m@beltran.work>
2022-12-19 12:27:18 +0100

    removed unused import

--- platform_channels/test/src/add_pet_details_test.dart
+++ platform_channels/test/src/add_pet_details_test.dart
@@ -6,7 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:platform_channels/main.dart' as app;
-import 'package:platform_channels/src/add_pet_details.dart';
 
 void main() {
   group('AddPetDetails tests', () {
