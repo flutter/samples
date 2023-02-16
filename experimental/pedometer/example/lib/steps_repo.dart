60fa82c4f6b1de7c89cb3913a8a0db5b6cf4fa3b
Hossein Yousefi <yousefi@google.com>
2023-02-16 14:22:40 +0100

    revert back the debugPrint

--- experimental/pedometer/example/lib/steps_repo.dart
+++ experimental/pedometer/example/lib/steps_repo.dart
@@ -2,6 +2,7 @@ import 'dart:ffi' as ffi;
 import 'dart:io';
 import 'dart:isolate';
 
+import 'package:flutter/foundation.dart';
 import 'package:intl/intl.dart';
 import 'package:jni/jni.dart' as jni;
 import 'package:pedometer/pedometer_bindings_generated.dart' as pd;
@@ -78,7 +79,7 @@ class _IOSStepsRepo implements StepsRepo {
   @override
   Future<List<Steps>> getSteps() async {
     if (!pd.CMPedometer.isStepCountingAvailable(lib)) {
-      print("Step counting is not available.");
+      debugPrint("Step counting is not available.");
       return [];
     }
 
