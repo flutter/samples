4d5acf97ca1574a1da8264d172193ece707616a0
Parker Lougheed <parlough@gmail.com>
2023-05-12 18:53:12 -0500

    Update samples index dependencies and constraints (#1816)
    Updates build dependencies as well as other constraints to more closely
    match versions used.
    
    Supersedes https://github.com/flutter/samples/pull/1814

--- web/samples_index/lib/src/data.g.dart
+++ web/samples_index/lib/src/data.g.dart
@@ -1,6 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of data;
+part of 'data.dart';
 
 // **************************************************************************
 // JsonSerializableGenerator
