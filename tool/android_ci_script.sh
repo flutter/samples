2319b7436bd8c4b2acf5ff22da82de5751c7e7db
Miguel Beltran <m@beltran.work>
2023-06-23 01:54:59 +0200

    fix Android ci job (#1904)

--- tool/android_ci_script.sh
+++ tool/android_ci_script.sh
@@ -37,7 +37,6 @@ do
     echo "== Testing '${PROJECT_NAME}' on Flutter's stable channel =="
     pushd "${PROJECT_NAME}"
 
-    gradle wrapper
     ./gradlew --stacktrace assembleDebug
     ./gradlew --stacktrace assembleRelease
 
