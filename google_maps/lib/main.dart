7a44a36d003a9cede391b50a6bd0167e7dac2125
AdamMusa <adammusa2222@gmail.com>
2023-06-29 01:29:36 +0100

    enable material m3 into google_maps example app

--- google_maps/lib/main.dart
+++ google_maps/lib/main.dart
@@ -1,12 +1,12 @@
 /*
  * Copyright 2019 Google LLC
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     https://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -38,10 +38,16 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Sydney'),
           backgroundColor: Colors.green[700],
+
         ),
         body: GoogleMap(
           onMapCreated: _onMapCreated,
