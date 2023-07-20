fdb4491916c03d92d379582c6fdfc578df9d2e57
Brett Morgan <brett.morgan@gmail.com>
2023-07-21 09:56:21 +1000

    Add everything back to beta CI

--- tool/flutter_ci_script_beta.sh
+++ tool/flutter_ci_script_beta.sh
@@ -22,11 +22,9 @@ declare -ar PROJECT_NAMES=(
     "code_sharing/server"
     "code_sharing/shared"
     "deeplink_store_example"
-    # TODO(DomesticMouse): Dependency 'fluent_ui' fails to compile. https://github.com/bdlukaa/fluent_ui/issues/865
-    # "desktop_photo_search/fluent_ui"
+    "desktop_photo_search/fluent_ui"
     "desktop_photo_search/material"
-    # TODO(DomesticMouse): The function can't be unconditionally invoked because it can be 'null'.
-    # "experimental/context_menus"
+    "experimental/context_menus"
     "experimental/federated_plugin/federated_plugin"
     "experimental/federated_plugin/federated_plugin/example"
     "experimental/federated_plugin/federated_plugin_macos"
@@ -35,24 +33,20 @@ declare -ar PROJECT_NAMES=(
     "experimental/federated_plugin/federated_plugin_windows"
     "experimental/linting_tool"
     "experimental/material_3_demo"
-    # TODO(DomesticMouse): dart format required
-    # "experimental/pedometer"
-    # TODO(DomesticMouse): dart format required
-    # "experimental/pedometer/example"
+    "experimental/pedometer"
+    "experimental/pedometer/example"
     "experimental/varfont_shader_puzzle"
     "experimental/web_dashboard"
     "flutter_maps_firestore"
     "form_app"
-    # TODO(DomesticMouse): The type 'AppLifecycleState' is not exhaustively matched by the switch cases since it doesn't match 'AppLifecycleState.hidden'.
-    # "game_template"
+    "game_template"
     "google_maps"
     "infinite_list"
     "ios_app_clip"
     "isolate_example"
     "jsonexample"
     "material_3_demo"
-    # TODO(DomesticMouse): The '!' will have no effect because the receiver can't be null.
-    # "navigation_and_routing"
+    "navigation_and_routing"
     "next_gen_ui_demo"
     "place_tracker"
     "platform_channels"
@@ -62,8 +56,7 @@ declare -ar PROJECT_NAMES=(
     "provider_shopper"
     "simple_shader"
     "simplistic_calculator"
-    # TODO(DomesticMouse): The named parameter 'onLiveTextInput' is required, but there's no corresponding argument.
-    # "simplistic_editor"
+    "simplistic_editor"
     "testing_app"
     "veggieseasons"
     "web_embedding/element_embedding_demo"
