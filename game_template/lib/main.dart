1ebbebad94eac1abab58d1e8d04cdb379a7dbe4d
Brett Morgan <brettmorgan@google.com>
2023-07-27 08:10:58 +1000

    Adding `const`s

--- game_template/lib/main.dart
+++ game_template/lib/main.dart
@@ -123,7 +123,7 @@ class MyApp extends StatelessWidget {
             GoRoute(
                 path: 'play',
                 pageBuilder: (context, state) => buildMyTransition<void>(
-                      key: ValueKey('play'),
+                      key: const ValueKey('play'),
                       child: const LevelSelectionScreen(
                         key: Key('level selection'),
                       ),
@@ -138,7 +138,7 @@ class MyApp extends StatelessWidget {
                       final level = gameLevels
                           .singleWhere((e) => e.number == levelNumber);
                       return buildMyTransition<void>(
-                        key: ValueKey('level'),
+                        key: const ValueKey('level'),
                         child: PlaySessionScreen(
                           level,
                           key: const Key('play session'),
@@ -164,7 +164,7 @@ class MyApp extends StatelessWidget {
                       final score = map['score'] as Score;
 
                       return buildMyTransition<void>(
-                        key: ValueKey('won'),
+                        key: const ValueKey('won'),
                         child: WinGameScreen(
                           score: score,
                           key: const Key('win game'),
