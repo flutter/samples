b4685fdf485be0b2ddbb8617371a457bde3d92d1
Brett Morgan <brett.morgan@gmail.com>
2023-10-20 09:27:21 +1000

    Drop `next_gen_ui_demo`

--- next_gen_ui_demo/lib/title_screen/title_screen.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-import '../main.dart';
-
-abstract class TitleScreenBase extends StatefulWidget {
-  const TitleScreenBase({super.key, required this.callback});
-  final ColorCallback callback;
-}
