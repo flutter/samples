17bd1e78f359e92a86901c2d9bc37359addbf43f
John Ryan <ryjohn@google.com>
2023-11-01 10:39:16 -0700

    Fix const analysis issue

--- navigation_and_routing/lib/src/screens/authors.dart
+++ navigation_and_routing/lib/src/screens/authors.dart
@@ -9,18 +9,19 @@ import '../data/library.dart';
 import '../widgets/author_list.dart';
 
 class AuthorsScreen extends StatelessWidget {
-  final String title = 'Authors';
+  final String title;
   final ValueChanged<Author> onTap;
 
   const AuthorsScreen({
     required this.onTap,
+    this.title = 'Authors',
     super.key,
   });
 
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(
-          title: const Text(title),
+          title: Text(title),
         ),
         body: AuthorList(
           authors: libraryInstance.allAuthors,
