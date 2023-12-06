f0e6da6d24c1a2bb0146678763306b4ebf5a5b16
Liam Appelbe <liama@google.com>
2023-12-06 00:58:58 -0800

    Simplify experimental pedometer example (#2104)
    Since ffigen added support for
    [`NativeCallable.listener`](https://api.flutter.dev/flutter/dart-ffi/NativeCallable/NativeCallable.listener.html)
    to its ObjC bindings, this example can be simplified. We can replace the
    `Dart_Port` logic with `ObjCBlock.listener`, which lets us get rid of
    most of the native code.
    
    We still need a small bit of native code to `retain` a reference to the
    callback's arguments before invoking the listener, otherwise the
    arguments may be ref counted and deleted before the Dart side of the
    callback is invoked. See https://github.com/dart-lang/native/issues/835

--- experimental/pedometer/src/pedometerHelper.h
+++ experimental/pedometer/src/pedometerHelper.h
@@ -1,15 +1,7 @@
 #import <CoreMotion/CoreMotion.h>
 #import <CoreMotion/CMPedometer.h>
 #import <Foundation/Foundation.h>
-#import <Foundation/NSDate.h>
 
-#include "dart-sdk/include/dart_api_dl.h"
-
-@interface PedometerHelper : NSObject
-
-+ (void) startPedometerWithPort: (Dart_Port) sendPort
-                      pedometer: (CMPedometer*) pedometer
-                          start: (NSDate*) start
-                            end: (NSDate*) end;
-
-@end
+// TODO(https://github.com/dart-lang/native/issues/835): Generate this wrapper
+// automatically.
+CMPedometerHandler wrapCallback(CMPedometerHandler callback);
