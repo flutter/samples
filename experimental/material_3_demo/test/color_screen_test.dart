a55a69605dd650cc5cfe453dc3dab45ac7dc0407
Qun Cheng <chengqunq@gmail.com>
2023-12-11 21:43:17 -0800

    make ColorScheme configurable

--- experimental/material_3_demo/test/color_screen_test.dart
+++ experimental/material_3_demo/test/color_screen_test.dart
@@ -4,6 +4,7 @@
 
 // ignore_for_file: avoid_types_on_closure_parameters
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:material_3_demo/color_palettes_screen.dart';
 import 'package:material_3_demo/main.dart';
@@ -65,11 +66,134 @@ void main() {
   });
 
   testWidgets('Color screen shows correct content', (tester) async {
-    await tester.pumpWidget(const MaterialApp(
-      home: Scaffold(body: Row(children: [ColorPalettesScreen()])),
-    ));
+    widgetSetup(tester, 1200);
+    addTearDown(tester.view.resetPhysicalSize);
+    await tester.pumpWidget(const App());
+
+    Finder colorPageIndicator = find.descendant(of: find.byType(NavigationRail),
+      matching: find.byIcon(Icons.format_paint_outlined));
+    expect(colorPageIndicator, findsOneWidget);
+    await tester.tap(colorPageIndicator);
+    await tester.pumpAndSettle();
     expect(find.text('Light ColorScheme'), findsOneWidget);
     expect(find.text('Dark ColorScheme'), findsOneWidget);
     expect(find.byType(ColorGroup, skipOffstage: false), findsNWidgets(16));
   });
+
+  testWidgets('Color screen shows correct content', (tester) async {
+    widgetSetup(tester, 1200);
+    addTearDown(tester.view.resetPhysicalSize);
+    await tester.pumpWidget(const App());
+
+    Finder colorPageIndicator = find.descendant(of: find.byType(NavigationRail),
+        matching: find.byIcon(Icons.format_paint_outlined));
+    expect(colorPageIndicator, findsOneWidget);
+    await tester.tap(colorPageIndicator);
+    await tester.pumpAndSettle();
+    expect(find.text('Light ColorScheme'), findsOneWidget);
+    expect(find.text('Dark ColorScheme'), findsOneWidget);
+    expect(find.byType(ColorGroup, skipOffstage: false), findsNWidgets(16));
+  });
+
+  testWidgets('ColorScheme are editable', (tester) async {
+    widgetSetup(tester, 1200);
+    addTearDown(tester.view.resetPhysicalSize);
+    await tester.pumpWidget(const App());
+
+    await updatePrimaryChipColor(tester, const Color(0xFF73A450), true);
+    expect(find.text('73a450'), findsOneWidget);
+
+    // Test if the chip color is changed to 73a450.
+    Finder primaryChip = find.descendant(of: find.widgetWithText(ColorChip, 'primary').first, matching: find.byType(Container));
+    Container container = tester.widget<Container>(primaryChip);
+    expect(container.color, const Color(0xFF73A450));
+  });
+
+  testWidgets('Light ColorScheme is configurable; changes can be applied to the widget page', (tester) async {
+    widgetSetup(tester, 1200);
+    addTearDown(tester.view.resetPhysicalSize);
+    await tester.pumpWidget(const App());
+
+    Color textColor(Finder text) {
+      return tester.renderObject<RenderParagraph>(text).text.style!.color!;
+    }
+
+    await updatePrimaryChipColor(tester, const Color(0xff123456), true);
+
+    // Go to the component screen
+    Finder componentPageIndicator = find.descendant(of: find.byType(NavigationRail),
+        matching: find.byIcon(Icons.widgets_outlined));
+    expect(componentPageIndicator, findsOneWidget);
+    await tester.tap(componentPageIndicator);
+    await tester.pumpAndSettle();
+
+    Finder elevatedButtonText = find.text('Elevated').first;
+    expect(textColor(elevatedButtonText), const Color(0xff123456));
+    Finder outlinedButtonText = find.text('Outlined').first;
+    expect(textColor(outlinedButtonText), const Color(0xff123456));
+    Finder textButtonText = find.text('Text').first;
+    expect(textColor(textButtonText), const Color(0xff123456));
+    final Material material = tester.widget<Material>(find.descendant(
+      of: find.byType(FilledButton).first,
+      matching: find.byType(Material),
+    ));
+    expect(material.color, const Color(0xff123456));
+  });
+
+  testWidgets('Dark ColorScheme is configurable; changes can be applied to the widget page', (tester) async {
+    widgetSetup(tester, 1200);
+    addTearDown(tester.view.resetPhysicalSize);
+    await tester.pumpWidget(const App());
+
+    Color textColor(Finder text) {
+      return tester.renderObject<RenderParagraph>(text).text.style!.color!;
+    }
+
+    // Switch to dark mode
+    await tester.tap(find.byIcon(Icons.dark_mode_outlined));
+    await tester.pumpAndSettle();
+    await updatePrimaryChipColor(tester, const Color(0xff654321), false);
+
+    // Go to the component screen
+    Finder componentPageIndicator = find.descendant(of: find.byType(NavigationRail),
+        matching: find.byIcon(Icons.widgets_outlined));
+    expect(componentPageIndicator, findsOneWidget);
+    await tester.tap(componentPageIndicator);
+    await tester.pumpAndSettle();
+
+    Finder elevatedButtonText = find.text('Elevated').first;
+    expect(textColor(elevatedButtonText), const Color(0xff654321));
+    Finder outlinedButtonText = find.text('Outlined').first;
+    expect(textColor(outlinedButtonText), const Color(0xff654321));
+    Finder textButtonText = find.text('Text').first;
+    expect(textColor(textButtonText), const Color(0xff654321));
+    final Material material = tester.widget<Material>(find.descendant(
+      of: find.byType(FilledButton).first,
+      matching: find.byType(Material),
+    ));
+    expect(material.color, const Color(0xff654321));
+  });
+}
+
+Future<void> updatePrimaryChipColor(WidgetTester tester, Color newColor, bool isLight) async {
+  Finder colorPageIndicator = find.descendant(of: find.byType(NavigationRail),
+      matching: find.byIcon(Icons.format_paint_outlined));
+  expect(colorPageIndicator, findsOneWidget);
+  await tester.tap(colorPageIndicator);
+  await tester.pumpAndSettle();
+  expect(isLight ? find.text('Light ColorScheme') : find.text('Dark ColorScheme'), findsOneWidget);
+
+  Finder primaryColorChip = isLight
+    ? find.widgetWithText(EditableColorChip, 'primary').first
+    : find.widgetWithText(EditableColorChip, 'primary').last;
+  await tester.tap(primaryColorChip);
+  await tester.pump();
+
+  expect(find.byType(AlertDialog), findsOneWidget);
+  await tester.enterText(find.byType(TextField), newColor.value.toRadixString(16));
+
+  // Tap on the barrier.
+  await tester.tapAt(const Offset(10.0, 10.0));
+  await tester.pumpAndSettle();
+  expect(find.byType(AlertDialog), findsNothing);
 }
