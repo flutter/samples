3429030d37be683f2e455ac18f7c31ddf1bd3c8f
Parker Lougheed <parlough@gmail.com>
2024-04-08 16:38:12 -0500

    Reenable simplistic_editor on CI

--- tool/flutter_ci_script_beta.sh
+++ tool/flutter_ci_script_beta.sh
@@ -60,8 +60,7 @@ declare -ar PROJECT_NAMES=(
     # "provider_shopper"
     "simple_shader"
     "simplistic_calculator"
-    # TODO(DomesticMouse): The method 'isSelectionWithinTextBounds' isn't defined for the type 'TextEditingController'
-    # "simplistic_editor"
+    "simplistic_editor"
     "testing_app"
     "veggieseasons"
     "web_embedding/element_embedding_demo"
