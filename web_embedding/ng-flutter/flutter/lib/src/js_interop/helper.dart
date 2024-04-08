6b8f18392d6677792fee2f2fb89fee902f6cef08
Parker Lougheed <parlough@gmail.com>
2024-04-08 16:22:25 -0500

    Clean up ng-flutter a bit (#2236)
    No functional change, just while I was verifying it still works with the
    latest Angular releases, completed some cleanup as well.

--- web_embedding/ng-flutter/flutter/lib/src/js_interop/helper.dart
+++ web_embedding/ng-flutter/flutter/lib/src/js_interop/helper.dart
@@ -4,7 +4,8 @@ import 'package:web/web.dart';
 /// Locates the root of the flutter app (for now, the first element that has
 /// a flt-renderer tag), and dispatches a JS event named [name] with [data].
 void broadcastAppEvent(String name, JSObject data) {
-  final HTMLElement? root = document.querySelector('[flt-renderer]') as HTMLElement?;
+  final HTMLElement? root =
+      document.querySelector('[flt-renderer]') as HTMLElement?;
   assert(root != null, 'Flutter root element cannot be found!');
 
   final eventDetails = CustomEventInit(detail: data);
