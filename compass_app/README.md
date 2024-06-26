170e10c383dd02dbecefa664ca45ae6586be4bcf
Eric Windmill <ewindmill@google.com>
2024-06-26 09:02:23 -0400

    initial

---
+++ compass_app/README.md
@@ -0,0 +1,3 @@
+# compass_app
+
+A new Flutter project.
---
+++ compass_app/android/app/src/main/kotlin/com/example/compass_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.compass_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
