92e5de84e79b75a80bd53b29ee7fd915d60042fd
Miguel Beltran <m@beltran.work>
2024-06-27 11:15:47 +0200

    implement search destination usecase

--- compass_app/app/lib/features/results/business/usecases/search_destination_usecase.dart
+++ compass_app/app/lib/features/results/business/usecases/search_destination_usecase.dart
@@ -1,12 +1,22 @@
 import 'package:compass_app/common/utils/response.dart';
 import 'package:compass_app/features/results/business/model/destination.dart';
-import 'package:compass_app/features/results/data/results_repository.dart';
+import 'package:compass_app/features/results/data/destination_repository.dart';
 
+/// Perform search over possible destinations
 class SearchDestinationUsecase {
+  SearchDestinationUsecase({required this.repository});
 
-  final ResultsRepository repository;
+  final DestinationRepository repository;
 
-  Future<Response<List<Destination>>> search({ String? region }) async {
+  Future<Response<List<Destination>>> search({ String? continent }) async {
+    bool filter(Destination destination) {
+      return (continent != null && destination.continent == continent);
+    }
 
+    final response = await repository.getDestinations();
+    return switch (response) {
+      Ok() => Response.ok(response.value.where(filter).toList()),
+      Error() => response,
+    };
   }
 }
--- compass_app/app/lib/features/results/data/destination_repository.dart
+++ compass_app/app/lib/features/results/data/destination_repository.dart
@@ -1,8 +1,10 @@
 import 'package:compass_app/common/utils/response.dart';
 import 'package:compass_app/features/results/business/model/destination.dart';
 
-class ResultsRepository {
+/// Data source with all possible destinations
+class DestinationRepository {
 
+  /// Get complete list of destinations
   Future<Response<List<Destination>>> getDestinations() {
     // TODO: Load some data
     return Future.value(Response.ok([]));
