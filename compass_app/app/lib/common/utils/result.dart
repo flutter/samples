813ded4e578339996ba4cb38e83d35bcc8266196
Miguel Beltran <m@beltran.work>
2024-07-01 19:02:39 +0200

    add Result class example

--- compass_app/app/lib/common/utils/result.dart
+++ compass_app/app/lib/common/utils/result.dart
@@ -1,4 +1,16 @@
 /// Utility class to wrap result data
+///
+/// Evaluate the result using a switch statement:
+/// ```dart
+/// switch (result) {
+///   case Ok(): {
+///     print(result.value);
+///   }
+///   case Error(): {
+///     print(result.error);
+///   }
+/// }
+/// ```
 sealed class Result<T> {
   const Result();
 
