c6ccc231b94cffae5ee0f7b69d8a33b946724a80
Miguel Beltran <m@beltran.work>
2024-07-01 13:15:13 +0200

    setup GoogleFonts

---
+++ compass_app/app/lib/common/themes/text_styles.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// Note: original Figma file uses Nikkei Maru
+// which is not available on GoogleFonts
+final cardTitleStyle = GoogleFonts.anton(
+  textStyle: const TextStyle(
+    fontWeight: FontWeight.w800,
+    fontSize: 15.0,
+    color: Colors.white,
+    letterSpacing: 1,
+  ),
+);
--- compass_app/app/lib/features/results/presentation/result_card.dart
+++ compass_app/app/lib/features/results/presentation/result_card.dart
@@ -1,3 +1,4 @@
+import 'package:compass_app/common/themes/text_styles.dart';
 import 'package:compass_app/features/results/business/model/destination.dart';
 import 'package:flutter/material.dart';
 
@@ -13,9 +14,36 @@ class ResultCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(10.0),
-      child: Image.network(
-        destination.imageUrl,
-        fit: BoxFit.fitHeight,
+      // TODO: Improve image loading and caching
+      child: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.network(
+            destination.imageUrl,
+            fit: BoxFit.fitHeight,
+          ),
+          Positioned(
+            bottom: 12.0,
+            left: 12.0,
+            right: 12.0,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  destination.name.toUpperCase(),
+                  style: cardTitleStyle,
+                ),
+                Wrap(
+                  direction: Axis.horizontal,
+                  children: destination.tags
+                      .map((e) => Chip(label: Text(e)))
+                      .toList(),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
