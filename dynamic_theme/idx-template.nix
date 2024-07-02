339404b55569f85f584ba9d57571d71afa12d8d6
Eric Windmill <ewindmill@google.com>
2024-07-02 10:44:38 -0400

    add flutter to path

--- dynamic_theme/idx-template.nix
+++ dynamic_theme/idx-template.nix
@@ -5,6 +5,9 @@
   ];
   # Shell script that produces the final environment
   bootstrap = ''
+    export HOME=/home/user
+    export PATH="$PATH":"$HOME/flutter/bin"
+
     cp -rf ${./.} "$out"
     chmod -R +w "$out"
     rm -rf "$out/.git" "$out/idx-template".{nix,json}
