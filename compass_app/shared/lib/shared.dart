e3432101435d3bb7dc15d7d625c1b9fe809d55f0
Miguel Beltran <m@beltran.work>
2024-07-15 16:19:35 +0200

    add compass_shared

---
+++ compass_app/shared/lib/shared.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/shared_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
---
+++ compass_app/shared/test/shared_test.dart
@@ -0,0 +1,16 @@
+import 'package:compass_shared/shared.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
