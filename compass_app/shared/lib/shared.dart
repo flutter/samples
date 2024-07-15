45522abb81228ef41670aecd82f0bd02f6301373
Miguel Beltran <m@beltran.work>
2024-07-15 16:45:12 +0200

    added freezed model classes

--- compass_app/shared/lib/shared.dart
+++ compass_app/shared/lib/shared.dart
@@ -1,8 +1,4 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library;
 
-export 'src/shared_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'src/model/continent/continent.dart';
+export 'src/model/destination/destination.dart';
