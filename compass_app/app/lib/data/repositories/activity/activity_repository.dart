2940bd1c904d0e1e79d67b41956b1504dfb54d57
Miguel Beltran <m@beltran.work>
2024-07-24 10:24:24 +0200

    cleanup

--- compass_app/app/lib/data/repositories/activity/activity_repository.dart
+++ compass_app/app/lib/data/repositories/activity/activity_repository.dart
@@ -5,8 +5,5 @@ import '../../../utils/result.dart';
 /// Data source for activities.
 abstract class ActivityRepository {
   /// Get activities by [Destination] ref.
-  ///
-  /// Returns [Result.error] with [ActivitiesNotFoundException]
-  /// if no Activities can be found.
   Future<Result<List<Activity>>> getByDestination(String ref);
 }
