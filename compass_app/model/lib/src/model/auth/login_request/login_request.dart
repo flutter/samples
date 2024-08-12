e8bf6ab36912b9e5f41cc63ebec88df029a870ae
Miguel Beltran <m@beltran.work>
2024-08-12 14:47:21 +0200

    add login request and response objects

---
+++ compass_app/model/lib/src/model/auth/login_request/login_request.dart
@@ -0,0 +1,20 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'login_request.freezed.dart';
+
+part 'login_request.g.dart';
+
+/// Simple data class to hold login request data.
+@freezed
+class LoginRequest with _$LoginRequest {
+  const factory LoginRequest({
+    /// Email address.
+    required String email,
+
+    /// Plain text password.
+    required String password,
+  }) = _LoginRequest;
+
+  factory LoginRequest.fromJson(Map<String, Object?> json) =>
+      _$LoginRequestFromJson(json);
+}
