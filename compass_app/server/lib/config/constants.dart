060dacaa7b88768ce3808334ca9fe2bdd7084fc8
Miguel Beltran <m@beltran.work>
2024-08-13 16:57:07 +0200

    format'

--- compass_app/server/lib/config/constants.dart
+++ compass_app/server/lib/config/constants.dart
@@ -6,7 +6,8 @@ class Constants {
   static const password = 'password';
 
   /// Token to be returned on successful login.
-  static const token = ' e1c37dfd973353b78bb71df050e2c6e72d53034e148920383968ae49b96f1fd2';
+  static const token =
+      ' e1c37dfd973353b78bb71df050e2c6e72d53034e148920383968ae49b96f1fd2';
 
   /// User id to be returned on successful login.
   static const userId = 'token';
