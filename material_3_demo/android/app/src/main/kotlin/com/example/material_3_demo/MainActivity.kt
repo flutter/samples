22cc7462ecc6eee172934197305239c7fe796c92
James Williams <66931+jwill@users.noreply.github.com>
2024-09-19 13:21:49 -0400

    Enables edge to edge support in material 3 demo.
    Edge to edge is coming by default to Flutter 3.26+ or so when the targetSdk is raised to Android 15(API 35). In the meantime, it can be done manually by adjusting the theme and setting `WindowCompat.setDecorFitsSystemWindows`.
    
    Fixes #1720 .

--- material_3_demo/android/app/src/main/kotlin/com/example/material_3_demo/MainActivity.kt
+++ material_3_demo/android/app/src/main/kotlin/com/example/material_3_demo/MainActivity.kt
@@ -1,6 +1,14 @@
 package com.example.material_3_demo
 
 import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle
+import androidx.core.view.WindowCompat
 
 class MainActivity: FlutterActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+    }
+
 }
