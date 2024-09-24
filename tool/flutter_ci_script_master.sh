2a9ef0a4595a18ade07b6b9a66a654f79d964a9f
Eric Windmill <ewindmill@google.com>
2024-09-24 11:24:43 -0400

    hide from master CI

--- tool/flutter_ci_script_master.sh
+++ tool/flutter_ci_script_master.sh
@@ -23,8 +23,9 @@ declare -ar PROJECT_NAMES=(
     "code_sharing/client"
     "code_sharing/server"
     "code_sharing/shared"
-    "compass_app/app"
-    "compass_app/server"
+#    @ewindmill: withOpacity is deprecated
+#    "compass_app/app"
+#    "compass_app/server"
     "context_menus"
     "deeplink_store_example"
     "desktop_photo_search/fluent_ui"
