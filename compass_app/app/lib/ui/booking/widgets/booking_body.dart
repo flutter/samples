566e356b8f41c2f7772bc919ff71c492c1c7b997
Eric Windmill <ewindmill@google.com>
2024-09-25 09:31:58 -0400

    add license header to files under /ui

--- compass_app/app/lib/ui/booking/widgets/booking_body.dart
+++ compass_app/app/lib/ui/booking/widgets/booking_body.dart
@@ -1,3 +1,7 @@
+// Copyright 2024 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
--- compass_app/app/lib/ui/core/themes/theme.dart
+++ compass_app/app/lib/ui/core/themes/theme.dart
@@ -1,3 +1,7 @@
+// Copyright 2024 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'colors.dart';
 import '../ui/tag_chip.dart';
 import 'package:flutter/material.dart';
