90cd385ef47548dcb7720781e805d5156a710604
Parker Lougheed <parlough@gmail.com>
2024-12-05 21:39:19 +0800

    [compass_app] Mark classes used for namespacing as `abstract final` (#2540)
    As they are just used for namespacing static properties, these classes
    shouldn't be extended, implemented, or instantiated.

--- compass_app/app/lib/routing/routes.dart
+++ compass_app/app/lib/routing/routes.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-class Routes {
+abstract final class Routes {
   static const home = '/';
   static const login = '/login';
   static const search = '/$searchRelative';
