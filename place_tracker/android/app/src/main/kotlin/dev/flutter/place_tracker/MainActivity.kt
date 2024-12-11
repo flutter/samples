b6629fde9f2a28122eb9276dffd38d2dfc4cd65d
Eric Windmill <eric@ericwindmill.com>
2024-12-11 15:02:14 -0500

    codelab_rebuild place_tracker

--- place_tracker/android/app/src/main/kotlin/dev/flutter/place_tracker/MainActivity.kt
+++ place_tracker/android/app/src/main/kotlin/dev/flutter/place_tracker/MainActivity.kt
@@ -2,5 +2,4 @@ package dev.flutter.place_tracker
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
--- place_tracker/ios/Runner/AppDelegate.swift
+++ place_tracker/ios/Runner/AppDelegate.swift
@@ -1,14 +1,12 @@
-import UIKit
 import Flutter
-import GoogleMaps
+import UIKit
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("YOUR KEY HERE")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
