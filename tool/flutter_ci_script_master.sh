e86b0a3b28a9ae076c81d2e6a0246cf86f74596e
Eric Windmill <eric@ericwindmill.com>
2025-02-12 17:40:33 -0500

    hide simplistic_editor on master

--- tool/flutter_ci_script_master.sh
+++ tool/flutter_ci_script_master.sh
@@ -59,7 +59,8 @@ declare -ar PROJECT_NAMES=(
     "rolodex"
     "simple_shader"
     "simplistic_calculator"
-    "simplistic_editor"
+    # TODO(ewindmill): 'scribble' is deprecated and shouldn't be used. Use stylusHandwriting instead.
+    # "simplistic_editor"
     "testing_app"
     "veggieseasons"
     "web_embedding/element_embedding_demo"
