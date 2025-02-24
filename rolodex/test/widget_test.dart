c787dd01a7e99a70d1cd8be57d546fc92aff0470
Mitchell Goodwin <58190796+MitchellGoodwin@users.noreply.github.com>
2025-02-24 10:06:50 -0800

    [Rolodex] Update fidelity of Contact list screens (#2588)
    This PR:
    
    - Updates the fidelity of the home screen and contact list screens to near full fidelity
    - Renames "Contact Lists" to "Contact Groups". We have multiple collections of contacts, so having lists of lists felt confusing
    - Adds more functionality to the data models
    
    Comparison of the two screens against native:
    
    | Native | Flutter |
    | --- | --- |
    | <img width="418" alt="Screenshot 2025-02-19 at 2 59 58 PM" src="https://github.com/user-attachments/assets/1cc53d19-6bb5-4782-82fa-1a62cd75fd51" /> | <img width="397" alt="Screenshot 2025-02-19 at 2 58 54 PM" src="https://github.com/user-attachments/assets/ddab75f2-4aec-4239-a736-690a3185c196" /> |
    | <img width="409" alt="Screenshot 2025-02-19 at 2 59 41 PM" src="https://github.com/user-attachments/assets/ee0d81ee-ae60-47ad-b071-266f39ce9b70" /> | <img width="402" alt="Screenshot 2025-02-19 at 2 58 45 PM" src="https://github.com/user-attachments/assets/88bf22f9-d8bd-40d7-a9ca-f2ded439de6c" /> |
    
    Notably the container widget on the first screen is a placeholder, until a Cupertino collapsable widget is made.

--- rolodex/test/widget_test.dart
+++ rolodex/test/widget_test.dart
@@ -1,18 +1,19 @@
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:rolodex/main.dart';
+import 'package:rolodex/screens/contacts.dart';
 
 void main() {
   testWidgets('Opens on all contacts page', (WidgetTester tester) async {
     await tester.pumpWidget(const RolodexApp());
 
-    expect(find.text('Contacts page'), findsOneWidget);
+    expect(find.byType(ContactListsPage), findsOneWidget);
     expect(find.text('Add List'), findsNothing);
 
     await tester.tap(find.text('Lists'));
     await tester.pumpAndSettle();
 
     expect(find.text('Add List'), findsOneWidget);
-    expect(find.text('Contacts page'), findsNothing);
+    expect(find.byType(ContactListsPage), findsNothing);
   });
 }
