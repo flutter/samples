a033128dd8667a18d2abd3f9141a26bbc6bdd406
louisehsu <louiseh0313@gmail.com>
2025-12-02 16:02:26 -0800

    init

---
+++ add_to_app/ios_content_resizing/ios_content_resizing/ios_content_resizing/ios_content_resizingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_content_resizingApp.swift
+//  ios_content_resizing
+//
+//  Created by Louise Hsu on 12/2/25.
+//
+
+import SwiftUI
+
+@main
+struct ios_content_resizingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ add_to_app/ios_dynamic_content_resize/ios_dynamic_content_resize/ios_dynamic_content_resizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_dynamic_content_resizeApp.swift
+//  ios_dynamic_content_resize
+//
+//  Created by Louise Hsu on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ios_dynamic_content_resizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
