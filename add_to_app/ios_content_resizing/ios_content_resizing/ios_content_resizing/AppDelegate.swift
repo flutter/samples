872d4912674892ae400a49a07ace9f176d756e49
louisehsu <louiseh0313@gmail.com>
2025-12-04 00:22:32 -0800

    works

--- add_to_app/ios_content_resizing/ios_content_resizing/ios_content_resizing/AppDelegate.swift
+++ add_to_app/ios_content_resizing/ios_content_resizing/ios_content_resizing/AppDelegate.swift
@@ -1,36 +1,14 @@
-//
-//  AppDelegate.swift
-//  ios_content_resizing
-//
-//  Created by Louise Hsu on 12/3/25.
-//
-
 import UIKit
+import Flutter
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: FlutterAppDelegate {
+    // Make the engine lazy to be created when it's first needed.
+    lazy var flutterEngine = FlutterEngine(name: "my_flutter_engine")
 
-
-
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
-    return true
-  }
-
-  // MARK: UISceneSession Lifecycle
-
-  func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-    // Called when a new scene session is being created.
-    // Use this method to select a configuration to create the new scene with.
-    return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-  }
-
-  func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-    // Called when the user discards a scene session.
-    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-  }
-
-
+    override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Pre-warms the FlutterEngine.
+        flutterEngine.run()
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
 }
-
--- add_to_app/ios_content_resizing/ios_content_resizing/ios_content_resizing/ViewController.swift
+++ add_to_app/ios_content_resizing/ios_content_resizing/ios_content_resizing/ViewController.swift
@@ -1,19 +1,53 @@
-//
-//  ViewController.swift
-//  ios_content_resizing
-//
-//  Created by Louise Hsu on 12/3/25.
-//
-
+import Flutter
 import UIKit
 
 class ViewController: UIViewController {
-
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+
+    showScollView()
   }
 
+  func showScollView() {
+    let scrollView = UIScrollView()
+    scrollView.isUserInteractionEnabled = true
 
+    let stackView = UIStackView()
+    stackView.axis = .vertical
+    stackView.distribution = .fill
+    stackView.translatesAutoresizingMaskIntoConstraints = false
+
+    let engine1 = FlutterEngine(name: "one")
+    engine1.run()
+
+    for index in 1...50 {
+      if (index == 10) {
+        let flutterViewController = FlutterViewController(engine: engine1, nibName: nil, bundle: nil)
+        flutterViewController.isAutoResizable = true
+        addChild(flutterViewController)
+        stackView.addArrangedSubview(flutterViewController.view)
+        flutterViewController.didMove(toParent: self)
+      } else {
+        let label = UILabel()
+        label.text = "Hello from iOS \(index)"
+        stackView.addArrangedSubview(label)
+      }
+    }
+    scrollView.addSubview(stackView)
+    scrollView.layoutIfNeeded()
+    self.view.addSubview(scrollView)
+
+    scrollView.translatesAutoresizingMaskIntoConstraints = false
+    scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+    scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+    scrollView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+    scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+
+    stackView.translatesAutoresizingMaskIntoConstraints = false
+    stackView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
+    stackView.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
+    stackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor).isActive = true
+    stackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor).isActive = true
+    stackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+  }
 }
-
