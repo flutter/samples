c586d727b98ece86b621d6727f32ce040668eeab
LouiseHsu <louiseh0313@gmail.com>
2025-12-10 08:38:54 -0800

    Add content resizing sample (#2756)
    Add a content resizing sample for iOS to support
    https://github.com/flutter/flutter/pull/177410
    
    ## Pre-launch Checklist
    
    - [x] I read the [Flutter Style Guide] _recently_, and have followed its
    advice.
    - [x] I signed the [CLA].
    - [x] I read the [Contributors Guide].
    - [x] I have added sample code updates to the [changelog].
    - [x] I updated/added relevant documentation (doc comments with `///`).
    
    If you need help, consider asking for advice on the #hackers-devrel
    channel on [Discord].
    
    <!-- Links -->
    [Flutter Style Guide]:
    https://github.com/flutter/flutter/blob/master/docs/contributing/Style-guide-for-Flutter-repo.md
    [CLA]: https://cla.developers.google.com/
    [Discord]:
    https://github.com/flutter/flutter/blob/master/docs/contributing/Chat.md
    [Contributors Guide]:
    https://github.com/flutter/samples/blob/main/CONTRIBUTING.md
    [changelog]: ../CHANGELOG.md
    
    ---------
    
    Co-authored-by: Eric Windmill <eric@ericwindmill.com>

---
+++ add_to_app/ios_content_resizing_example/ios_app/ios_content_resizing_example/ios_content_resizing_example/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ios_content_resizing_example
+//
+//  Created by Louise Hsu on 12/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ add_to_app/ios_dynamic_content_resize/ios_dynamic_content_resizeUITests/ios_dynamic_content_resizeUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  ios_dynamic_content_resizeUITestsLaunchTests.swift
+//  ios_dynamic_content_resizeUITests
+//
+//  Created by Louise Hsu on 11/11/25.
+//
+
+import XCTest
+
+final class ios_dynamic_content_resizeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
