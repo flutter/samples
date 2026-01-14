f4c8644cb9d4d59d32093dcd3e0b9cacd166eec9
spakkan <sinan.pakkan@gmail.com>
2026-01-14 15:47:37 +0300

    pubspec.yaml firebase lines commented out for build on Windows target. Other files automatically updated to exclude related firebase lines.
    This version builds on TRM laptop.

--- game_template/windows/flutter/generated_plugin_registrant.cc
+++ game_template/windows/flutter/generated_plugin_registrant.cc
@@ -7,11 +7,8 @@
 #include "generated_plugin_registrant.h"
 
 #include <audioplayers_windows/audioplayers_windows_plugin.h>
-#include <firebase_core/firebase_core_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   AudioplayersWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("AudioplayersWindowsPlugin"));
-  FirebaseCorePluginCApiRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("FirebaseCorePluginCApi"));
 }
