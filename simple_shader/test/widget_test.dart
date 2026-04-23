94a02b326d8516145c11d26c3263a5962ad66694
Andy Wolff <awolff@google.com>
2026-04-23 10:49:51 -0700

    Add copyright to source files in simple_shader and simple_sdf

--- simple_shader/test/widget_test.dart
+++ simple_shader/test/widget_test.dart
@@ -1,3 +1,7 @@
+// Copyright 2024 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:simple_shader/main.dart';
 
